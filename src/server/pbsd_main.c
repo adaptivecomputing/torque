f687f5c261680481095d35dd7f29ae7d32f2f712
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2006-09-28 03:40:17 +0000

    incorrect usage of set_task in src/server/pbsd_main.c

--- src/server/pbsd_main.c
+++ src/server/pbsd_main.c
@@ -980,23 +980,24 @@ int main(
   /* do not check nodes immediately as they will initially be marked 
      down unless they have already reported in */
 
+  when = server.sv_attr[(int)SRV_ATR_check_rate].at_val.at_long + time_now;
   if (svr_totnodes > 1024)
     {
     /* for large systems, give newly reported nodes more time before 
        being marked down while pbs_moms are intialy reporting in */
 
-    set_task(WORK_Immed,time_now + svr_totnodes / 12,check_nodes,NULL);
+    set_task(WORK_Timed,when + svr_totnodes / 12,check_nodes,NULL);
     }
   else
     {
-    set_task(WORK_Immed,time_now + 60,check_nodes,NULL);
+    set_task(WORK_Timed,when,check_nodes,NULL);
     }
 
   /* Just check the nodes with check_nodes above and don't ping anymore. */
 
   set_task(WORK_Timed,time_now + 5,ping_nodes,NULL); 
 
-  set_task(WORK_Immed,time_now + 5,check_log,NULL);
+  set_task(WORK_Timed,time_now + 5,check_log,NULL);
 
   /*
    * Now at last, we are ready to do some batch work.  The
