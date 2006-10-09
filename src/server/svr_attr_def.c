2d05bb0022dfb2e5fb436c8ed13ae88f683a3e86
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2006-10-09 23:50:17 +0000

    comment typo in ./src/server/svr_attr_def.c

--- src/server/svr_attr_def.c
+++ src/server/svr_attr_def.c
@@ -161,7 +161,7 @@ attribute_def svr_attr_def[] = {
 	PARENT_TYPE_SERVER,
     },
 
-/* SRV_ATR_MAX_running */
+/* SRV_ATR_max_running */
     {	ATTR_maxrun,		/* "max_running" */
 	decode_l,
 	encode_l,
