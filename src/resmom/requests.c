b9136a7d759508530211bd30362c7e7b4926abbf
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2006-10-21 21:54:41 +0000

    last rcperr fix still left an opening

--- src/resmom/requests.c
+++ src/resmom/requests.c
@@ -2459,7 +2459,6 @@ static int sys_copy(
     else 
       {
       int fd;
-      int flags;
 
       /* child - exec the copy command */
 
@@ -2469,12 +2468,9 @@ static int sys_copy(
 
       /* redirect stderr to make error from rcp available to MOM */
 
-      if (loop == 1)
-        flags=O_WRONLY|O_CREAT|O_EXCL;
-      else
-        flags=O_WRONLY|O_APPEND;
+      unlink(rcperr);  /* likely failing, ignore */
 
-      if ((fd = open(rcperr,flags,0644)) < 0) 
+      if ((fd = open(rcperr,O_RDWR|O_CREAT|O_EXCL,0644)) < 0) 
         {
         sprintf(log_buffer,"can't open %s, error = %d",
           rcperr,errno);
