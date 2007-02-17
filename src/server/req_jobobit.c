322b12da144a6af684466b564c901578feb39d24
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2007-02-17 01:51:19 +0000

    also clear session_id on job requeue

--- src/server/req_jobobit.c
+++ src/server/req_jobobit.c
@@ -1422,10 +1422,13 @@ void on_job_rerun(
       if ((pjob->ji_qs.ji_svrflags & JOB_SVFLG_HOTSTART) == 0) 
         {
         /* in case of server shutdown, don't clear exec_host */
-        /* will use it on hotstart when next comes up	     */
+        /* or session_id will use it on hotstart when next comes up */
 
         job_attr_def[(int)JOB_ATR_exec_host].at_free(
           &pjob->ji_wattr[(int)JOB_ATR_exec_host]);
+
+        job_attr_def[(int)JOB_ATR_session_id].at_free(
+          &pjob->ji_wattr[(int)JOB_ATR_session_id]);
         }
 
       pjob->ji_modified = 1;	/* force full job save */
