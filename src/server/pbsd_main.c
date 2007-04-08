9b9b554e82f259839a8264f6cefb8c1c425db068
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2007-04-08 03:02:17 +0000

    move pbs_server's getuid==0 zero check after options processing

--- src/server/pbsd_main.c
+++ src/server/pbsd_main.c
@@ -467,16 +467,6 @@ int main(
 
   msg_daemonname = strdup(pbs_current_user);
 
-  /* if we are not running with real and effective uid of 0, forget it */
-
-  if ((getuid() != 0) || (geteuid() != 0)) 
-    {
-    fprintf(stderr,"%s: must be run by root\n", 
-      ProgName);
-
-    return(1);
-    }
-
   /* set standard umask */
 
   umask(022);
@@ -869,6 +859,16 @@ int main(
       argv[0]);
 
     exit(1);
+    }
+
+  /* if we are not running with real and effective uid of 0, forget it */
+
+  if ((getuid() != 0) || (geteuid() != 0)) 
+    {
+    fprintf(stderr,"%s: must be run by root\n", 
+      ProgName);
+
+    return(1);
     }
 
   /* make sure no other server is running with this home directory */
