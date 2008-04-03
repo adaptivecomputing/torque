655eadf0cf0e22bfb91f0e5e110be243099659d2
josh <josh@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2008-04-03 15:13:20 +0000

    FEATURE:	added the ability for TORQUE to assign hosts to a job based on the contents of the "rq_extend" string--this is used by schedulers like Moab to control where a

--- src/server/req_runjob.c
+++ src/server/req_runjob.c
@@ -1215,10 +1215,12 @@ static job *chk_job_torun(
     {
     /* job has not run before or need not run there again */
     /* reserve nodes and set new exec_host */
-
-    if (prun->rq_destin == 0) 
+    if ((prun->rq_destin == NULL) || (prun->rq_destin[0] == '\0'))
       {
-      rc = assign_hosts(pjob,NULL,1,FailHost,EMsg);  /* inside chk_job_torun() */
+      /* it is possible for the scheduler to pass a hostlist using the rq_extend field--we should use it as the given list
+       * as an alternative to rq_destin */
+
+      rc = assign_hosts(pjob,preq->rq_extend,1,FailHost,EMsg);  /* inside chk_job_torun() */
       } 
     else 
       {
