226d67aac4cf5f3248036d276d409b71537f6e5a
ataufer <ataufer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2008-06-24 23:02:43 +0000

    b - moved reset of ji_destin so it happens regardless of brp_code

--- src/resmom/catch_child.c
+++ src/resmom/catch_child.c
@@ -1394,6 +1394,10 @@ static void obit_reply(
     if ((pjob->ji_qs.ji_substate == JOB_SUBSTATE_OBIT) &&
         (pjob->ji_momhandle == sock)) 
       {
+      /* Clear out destination so we know job is not on mom any more */
+       
+      pjob->ji_qs.ji_destin[0] = '\0';
+
       switch (preq->rq_reply.brp_code) 
         {
         case PBSE_NONE:
@@ -1402,10 +1406,6 @@ static void obit_reply(
 
           pjob->ji_qs.ji_substate = JOB_SUBSTATE_EXITED;
           
-          /* Clear out destination so we know job is not on mom any more */
-          
-          pjob->ji_qs.ji_destin[0] = '\0';
-
           job_save(pjob,SAVEJOB_QUICK);
 
           if (LOGLEVEL >= 4)
