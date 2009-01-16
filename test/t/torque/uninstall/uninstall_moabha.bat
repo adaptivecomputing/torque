2f4bacd2e5630317bb5fb8d2abfb7f5de259334a
jdayley <jdayley@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2009-01-16 20:21:03 +0000

    Added the uninstall_moabhaha.bat file for now.  This may need to be placed in the moab tests

---
+++ test/t/torque/uninstall/uninstall_moabha.bat
@@ -0,0 +1,26 @@
+#! /usr/bin/perl 
+use strict;
+use warnings;
+use CRI::Test;
+plan('no_plan');
+setDesc('Uninstall Torque from Primary and Fallback Servers for Moab HA');
+
+my $testbase = $props->get_property('test.base') . "torque/uninstall";
+my $fallback = $props->get_property('fallback.hostname');
+
+my @testList = (
+    "$testbase/shutdown.t",
+    "$testbase/remove.t",
+);
+
+foreach my $test (@testList)
+{
+    execute_tests($test)
+	or die "Torque uninstall failed on Primary server due to $test";
+}
+
+foreach my $test (@testList)
+{
+    runCommandSsh($fallback, $test)
+	and die "Torque uninstall failed on Fallback server due to $test";
+}
