18cca6416f3ef57195f2b4a0bbe60719d4f5865d
jdayley <jdayley@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2009-01-16 20:19:49 +0000

    Added place holders for the command snap and full batch files

---
+++ test/t/torque/commands/snap.bat
@@ -0,0 +1,34 @@
+#!/usr/bin/perl 
+
+use CRI::Test;
+plan('no_plan');
+setDesc('SNAP Torque Compatibility Tests');
+use strict;
+use warnings;
+
+my $testbase = $props->get_property('test.base') . "torque/commands";
+execute_tests(
+#              "$testbase/momctl/snap.bat",
+#              "$testbase/qalter/snap.bat",
+#              "$testbase/qchkpt/snap.bat",
+#              "$testbase/qdel/snap.bat",
+#              "$testbase/qhold/snap.bat",
+#              "$testbase/qmgr/snap.bat",
+#              "$testbase/qrerun/snap.bat",
+#              "$testbase/qrls/snap.bat",
+#              "$testbase/qrun/snap.bat",
+#              "$testbase/qsig/snap.bat",
+#              "$testbase/qstat/snap.bat",
+#              "$testbase/qsub/snap.bat",
+#              "$testbase/qterm/snap.bat",
+#              "$testbase/tracejob/snap.bat",
+             ); 
+
+# Tests that can make the environment unstable if they fail.  We want to limit
+# their impact on the test suite
+execute_tests(
+#              "$testbase/pbsnodes/snap.bat",
+#              "$testbase/pbs_sched/snap.bat",
+#              "$testbase/pbs_mom/snap.bat",
+#              "$testbase/pbs_server/snap.bat",
+             );
