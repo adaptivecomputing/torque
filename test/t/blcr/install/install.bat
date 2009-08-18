a9f2608bc2db0026d6f22aabc8e2c83cea0aecf4
jdayley <jdayley@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2009-08-18 21:53:10 +0000

    TESTING: Removed the environment check from the blcr install batch script

--- test/t/blcr/install/install.bat
+++ test/t/blcr/install/install.bat
@@ -21,9 +21,9 @@ execute_tests(
         "$testbase/040_make_blcr.t",
         "$testbase/050_make_insmod_blcr.t",
 #        "$testbase/060_make_check_blcr.t",
-        "$testbase/070_make_install_blcr.t",
+        "$testbase/070_make_install_blcr.t", 
         );
 
 execute_tests(
-        "$testbase/environment_config.bat",
+#        "$testbase/environment_config.bat", # This can be inconsistant
         );
