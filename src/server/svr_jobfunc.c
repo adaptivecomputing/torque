2b3c7c7048ce2f844385362bc555a8a829256ec4
glen <glen@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2009-08-20 01:53:16 +0000

    merging in fix for bugzilla bug #17

--- src/server/svr_jobfunc.c
+++ src/server/svr_jobfunc.c
@@ -444,11 +444,10 @@ int svr_enquejob(
       {
       pjob->ji_wattr[(int)JOB_ATR_etime].at_val.at_long = time_now;
       pjob->ji_wattr[(int)JOB_ATR_etime].at_flags |= ATR_VFLAG_SET;
-
-      /* notify the scheduler we have a new job */
-
-      svr_do_schedule = SCH_SCHEDULE_NEW;
       }
+      
+    /* notify the scheduler we have a new job */
+    svr_do_schedule = SCH_SCHEDULE_NEW;
     }
   else if (pque->qu_qs.qu_type == QTYPE_RoutePush)
     {
