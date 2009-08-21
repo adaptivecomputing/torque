6e3bda894975cb9cd0658b33c6915d89b4d2078d
jdayley <jdayley@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2009-08-21 21:14:58 +0000

    TESTING: Added a check to see if blcr is already installed. If so don't reinstall blcr

--- test/t/blcr/reinstall.bat
+++ test/t/blcr/reinstall.bat
@@ -14,8 +14,26 @@ setDesc("Reinstall 'Berkley Lab Checkpoint/Restart (BLCR) for Linux'");
 
 my $testbase = $FindBin::Bin;
 
+# Check to see if blcr is alread installed
+my $blcr_bin = $props->get_property('blcr.home.dir') . "/bin";
+my $cr_run   = "$blcr_bin/cr_run";
+
+if (-e $cr_run)
+  {
+
+  my %result = runCommand($cr_run);
+
+  if ($result{ 'EXIT_CODE' } == 0)
+    {
+
+    pass("BLCR is already installed.  Skipping the install"); 
+    exit 0;
+
+    } # END if ($result{ 'EXIT_CODE' }, '!=', 0)
+
+  } # END if (-e $cr_run)
 
 execute_tests(
-        "$testbase/uninstall/uninstall.bat",
-        "$testbase/install/install.bat",
-        );
+              "$testbase/uninstall/uninstall.bat",
+              "$testbase/install/install.bat",
+             );
