9552d4a6e4bc1b5c79cc88587398f036b8cf9272
itobler <itobler@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2009-10-06 18:19:37 +0000

    small test fix

--- test/t/torque/reinstall.bat
+++ test/t/torque/reinstall.bat
@@ -1,21 +1,24 @@
 #!/usr/bin/perl 
-
 use strict;
 use warnings;
 
 use FindBin;
 use lib "$FindBin::Bin/../../lib/";
 
-
 use CRI::Test;
-
 plan('no_plan');
-setDesc("Reinstall Latest Torque (from subversion) to use BLCR checkpointing");
+setDesc("Reinstall Latest SVN Torque to use BLCR checkpointing");
 
-my $testbase = "$FindBin::Bin/../";
+my $testbase = "$FindBin::Bin/..";
 
-execute_tests(
-               "${testbase}blcr/reinstall.bat",
-               "${testbase}torque/uninstall/uninstall.bat",
-               "${testbase}torque/install/install_latest_rcs.bat",
-             );
+my @testList = (
+    "$testbase/blcr/reinstall.bat",
+    "$testbase/torque/uninstall/uninstall.bat",
+    "$testbase/torque/install/install_latest_rcs.bat",
+);
+
+foreach( @testList )
+{
+    execute_tests($_)
+	or die "Installation failed due to $_";
+}
