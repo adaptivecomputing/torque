c9f1ea7f73bfa69ff5d2c8cd1f443b3c19c74650
jdayley <jdayley@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2010-06-17 17:30:02 +0000

    These files had duplicate code (copy and paste error?).  Each was running through the install steps twice.

--- test/t/torque/install/remote_install_blcr.bat
+++ test/t/torque/install/remote_install_blcr.bat
@@ -23,28 +23,3 @@ execute_tests(
 "$testbase/create_torque_conf.t",
 "$testbase/check_blcr.t",
 );
-#!/usr/bin/perl 
-
-use strict;
-use warnings;
-
-use FindBin;
-use lib "$FindBin::Bin/../../../lib/";
-
-use CRI::Test;
-plan('no_plan');
-setDesc('Remote Install Torque with BLCR');
-
-my $testbase = $FindBin::Bin;
-
-execute_tests(
-"$testbase/configure.t",
-"$testbase/make_clean.t",
-"$testbase/make.t",
-"$testbase/make_install.t",
-"$testbase/setup.t",
-"$testbase/config_mom.t",
-"$testbase/cp_chkpt_scripts.t",
-"$testbase/create_torque_conf.t",
-"$testbase/check_blcr.t",
-);
