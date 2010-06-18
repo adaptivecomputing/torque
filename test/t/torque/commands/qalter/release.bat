60ff066ad776a8ea31e9ea4a5d2a3c240a15b867
jdayley <jdayley@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2010-06-18 22:27:00 +0000

    Commented out the qalter_a.t test. See RT8017.

--- test/t/torque/commands/qalter/release.bat
+++ test/t/torque/commands/qalter/release.bat
@@ -16,7 +16,7 @@ execute_tests("$testbase/setup.t")
   or die("Couldn't setup for qalter tests!");
 
 execute_tests(
-              "$testbase/qalter_a.t",
+# RT8017              "$testbase/qalter_a.t",
               "$testbase/qalter_cA.t",
               "$testbase/qalter_c.t",
               "$testbase/qalter_e.t",
