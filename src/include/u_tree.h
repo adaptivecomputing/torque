94c91f62b0d963e28c35e437650ea452ceba04d4
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2010-07-22 21:53:59 +0000

    Adding u_tree.h to trunk from NUMA branch

---
+++ src/include/u_tree.h
@@ -0,0 +1,29 @@
+#ifndef __U_TREE_H__
+#define __U_TREE_H__
+
+#include <stdlib.h>
+#include <pbs_nodes.h>
+
+
+struct AvlNode
+  {
+  u_long	 	  key;
+  unsigned short  port;
+  struct pbsnode *pbsnode;
+  struct AvlNode  *left;
+  struct AvlNode  *right;
+  int			  height;
+  };
+
+typedef struct AvlNode *AvlTree;
+typedef struct AvlNode *NodeEntry;
+
+/* u_tree prototypes */
+AvlTree AVL_delete_node( u_long key, uint16_t port, AvlTree tree);
+struct pbsnode *AVL_find(u_long key, uint16_t port, AvlTree tree);
+AvlTree AVL_insert( u_long key, uint16_t port, struct pbsnode *node, AvlTree tree );
+int AVL_list( AvlTree tree, char *Buf, uint32_t BufSize );
+int AVL_is_in_tree(u_long key, uint16_t port, AvlTree tree);
+
+
+#endif /* __U_TREE_H__*/
