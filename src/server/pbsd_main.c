fca1c4fd0221275bf321c508381ee2b732d267fd
knielson <knielson@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-03-18 16:00:47 +0000

    removed redundant variable assignment in lock_out_ha for UseFLock

--- src/server/pbsd_main.c
+++ src/server/pbsd_main.c
@@ -2535,10 +2535,7 @@ static void lock_out_ha()
 
     NumChecks++;
 
-    if (is_ha_lock_file_valid(HALockFile) == FALSE)
-      {
-      UseFLock = TRUE;
-      }
+    UseFLock = is_ha_lock_file_valid(HALockFile);
 
     if (UseFLock == TRUE)
       {
