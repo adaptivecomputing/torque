137aece91c54f2ff3128623fa1eb029e5feac245
ataufer <ataufer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-05-05 18:49:12 +0000

    Backout R 4648 until we can get some makefile issues resolved.

--- src/resmom/pbs_demux.c
+++ src/resmom/pbs_demux.c
@@ -98,9 +98,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#ifdef ENABLE_BLCR
-#include <libcr.h>
-#endif /* ENABLE_BLCR */
 
 #if defined(FD_SET_IN_SYS_SELECT_H)
 #  include <sys/select.h>
@@ -195,17 +192,8 @@ void readit(
   return;
   }  /* END readit() */
 
-#ifdef ENABLE_BLCR
-static int demux_callback(void* arg)
-{
-    fflush(stdout);
-    fflush(stderr);
 
-    cr_checkpoint(CR_CHECKPOINT_OMIT);
 
-    return 0;
-}
-#endif /* ENABLE_BLCR */
 
 int main(
 
@@ -224,16 +212,6 @@ int main(
   fd_set selset;
 
   struct routem *routem;
-
-#ifdef ENABLE_BLCR
-  if (cr_init() < 0)
-    {
-    perror("Failed to initialize BLCR.");
-    exit(5);
-    }
-
-  (void)cr_register_callback(demux_callback, NULL, CR_THREAD_CONTEXT);
-#endif /* ENABLE_BLCR */
 
   parent = getppid();
 
