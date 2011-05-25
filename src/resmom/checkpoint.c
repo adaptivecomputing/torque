998844eb30709ad4096a3de28e4aee598f03a904
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-05-25 16:30:52 +0000

    Fix an error that will cause us to unnecessarily chmod a file when checkpointing. && is used instead of &

--- src/resmom/checkpoint.c
+++ src/resmom/checkpoint.c
@@ -224,8 +224,8 @@ mom_checkpoint_execute_job(job *pjob, char *shell, char *arg[], struct var_table
  * This routine is called from the mom startup code.
  * @see setup_program_environment
  */
-int
-mom_checkpoint_init(void)
+int mom_checkpoint_init(void)
+
   {
   int   c = 0;
   char *path_checkpt_tmp;
@@ -246,7 +246,7 @@ mom_checkpoint_init(void)
     /* set permissions on default checkpoint path, if needed */
     struct stat sb;
     
-    if ((stat(path_checkpoint, &sb) == 0) && ((sb.st_mode && 01777) != 01777)) 
+    if ((stat(path_checkpoint, &sb) == 0) && ((sb.st_mode & 01777) != 01777)) 
       {
       chmod(path_checkpoint, 01777);
       }
@@ -274,8 +274,10 @@ mom_checkpoint_init(void)
 /*========================================================================*/
 
 
-void
-mom_checkpoint_set_directory_path(char *str)
+void mom_checkpoint_set_directory_path(
+    
+  char *str)
+
   {
   char *cp;
 
