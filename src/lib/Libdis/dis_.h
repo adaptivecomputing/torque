331628a1851912303536dff62c64c6535414ea51
jrosenquist <jrosenquist@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-07-25 17:32:39 +0000

    Fixed signature changes

--- src/lib/Libdis/dis_.h
+++ src/lib/Libdis/dis_.h
@@ -1,4 +1,4 @@
-#include <license_pbs.h> /* See here for the software license */
+#include "license_pbs.h" /* See here for the software license */
 #include <limits.h>
 #include <stddef.h>
 
@@ -14,34 +14,34 @@ void disi10l_();
 void disiui_(void);
 double disp10d_(int expon);
 dis_long_double_t disp10l_(int expon);
-char *disrcs(int stream, int rpp, size_t *nchars, int *retval);
-double disrd(int stream, int rpp, int *retval);
-float disrf(int stream, int rpp, int *retval);
-int disrfcs(int stream,int rpp,size_t *nchars,size_t achars, char *value);
-int disrfst(int stream, int rpp, size_t achars, char *value);
-dis_long_double_t disrl(int stream, int rpp, int *retval);
-int disrl_(int stream, int rpp, dis_long_double_t *ldval, unsigned *ndigs,
+char *disrcs(int stream, size_t *nchars, int *retval);
+double disrd(int stream, int *retval);
+float disrf(int stream, int *retval);
+int disrfcs(int stream, size_t *nchars,size_t achars, char *value);
+int disrfst(int stream, size_t achars, char *value);
+dis_long_double_t disrl(int stream, int *retval);
+int disrl_(int stream, dis_long_double_t *ldval, unsigned *ndigs,
     unsigned *nskips, unsigned sigd, unsigned count);
-signed char disrsc( int stream, int rpp, int *retval);
-int disrsi(int stream, int rpp, int *retval);
-int disrsi_(int stream, int rpp, int *negate, unsigned *value, unsigned count);
-long disrsl(int stream, int rpp, int *retval);
-int disrsl_(int stream, int rpp, int *negate, unsigned long *value,
+signed char disrsc( int stream, int *retval);
+int disrsi(int stream, int *retval);
+int disrsi_(int stream, int *negate, unsigned *value, unsigned count);
+long disrsl(int stream, int *retval);
+int disrsl_(int stream, int *negate, unsigned long *value,
     unsigned long count);
-short disrss(int stream, int rpp, int *retval);
-char *disrst(int stream, int rpp, int *retval);
-unsigned char disruc(int stream, int rpp, int *retval);
-unsigned disrui(int stream, int rpp, int *retval);
-unsigned long disrul(int stream, int rpp, int *retval);
-unsigned short disrus(int stream, int rpp, int *retval);
-int diswcs(int stream, int rpp, const char *value, size_t nchars);
-int diswf(int stream, int rpp, double value);
-int diswl_(int stream, int rpp, dis_long_double_t value, unsigned ndigs);
-int diswsi(int stream, int rpp, int value);
-int diswsl(int stream, int rpp, long value);
-int diswui(int stream, int rpp, unsigned value);
-int diswui_(int stream, int rpp, unsigned value);
-int diswul(int stream, int rpp, unsigned long value);
+short disrss(int stream, int *retval);
+char *disrst(int stream, int *retval);
+unsigned char disruc(int stream, int *retval);
+unsigned disrui(int stream, int *retval);
+unsigned long disrul(int stream, int *retval);
+unsigned short disrus(int stream, int *retval);
+int diswcs(int stream, const char *value, size_t nchars);
+int diswf(int stream, double value);
+int diswl_(int stream, dis_long_double_t value, unsigned ndigs);
+int diswsi(int stream, int value);
+int diswsl(int stream, long value);
+int diswui(int stream, unsigned value);
+int diswui_(int stream, unsigned value);
+int diswul(int stream, unsigned long value);
 
 extern unsigned dis_dmx10;
 extern double *dis_dp10;
