4d2db9e91b2214622e46da70d7dde7f2a151d544
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-08-04 16:46:42 +0000

    add a read_nonblocking_socket to scaffolding

--- src/lib/Libnet/test/port_forwarding/scaffolding.c
+++ src/lib/Libnet/test/port_forwarding/scaffolding.c
@@ -8,3 +8,9 @@ ssize_t write_nonblocking_socket( int fd,  const void *buf,  ssize_t count)
   fprintf(stderr, "The call to write_nonblocking_socket needs to be mocked!!\n");
   exit(1);
   }
+
+ssize_t read_nonblocking_socket(int fd, void *buf, ssize_t count)
+  {
+  fprintf(stderr, "The call to read_nonblocking_socket needs to be mocked!!\n");
+  exit(1);
+  }
