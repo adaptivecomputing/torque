b6acade071c87b0f1c0af340ad6a63f589564ea5
itobler <itobler@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-10-07 17:59:33 +0000

    TESTING: TRQ-404 resolved

--- test/t/torque/commands/pbsnodes/release.bat
+++ test/t/torque/commands/pbsnodes/release.bat
@@ -19,9 +19,9 @@ execute_tests("$testbase/setup.t")
 execute_tests(
     "$testbase/pbsnodes.t",
     "$testbase/pbsnodes_a.t",
-# JR-TRQ-404    "$testbase/pbsnodes_c.t",
+    "$testbase/pbsnodes_c.t",
 #    "$testbase/pbsnodes_d.t", # Not implemented yet
-# JR-TRQ-404    "$testbase/pbsnodes_l.t",
+    "$testbase/pbsnodes_l.t",
     "$testbase/pbsnodes_o.t",
 #    "$testbase/pbsnodes_p.t", # Not implemented yet
     "$testbase/pbsnodes_q.t",
@@ -33,10 +33,10 @@ execute_tests(
     "$testbase/pbsnodes_s_q.t",
     "$testbase/pbsnodes_x_q.t",
     "$testbase/pbsnodes_l_n.t",
-# JR-TRQ-404    "$testbase/pbsnodes_o_cN.t",
+    "$testbase/pbsnodes_o_cN.t",
     "$testbase/pbsnodes_s_a.t",
-# JR-TRQ-404    "$testbase/pbsnodes_s_c.t",
-# JR-TRQ-404    "$testbase/pbsnodes_s_l.t",
+    "$testbase/pbsnodes_s_c.t",
+    "$testbase/pbsnodes_s_l.t",
     "$testbase/pbsnodes_s_x.t",
     "$testbase/pbsnodes_s_a_q.t",
     "$testbase/pbsnodes_s_c_q.t",
