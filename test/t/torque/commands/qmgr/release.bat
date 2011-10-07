965cdb46d57df53a7ddceb56f3d9a080a900e27f
itobler <itobler@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-10-07 20:34:51 +0000

    TESTING: resolved TRQ-475

--- test/t/torque/commands/qmgr/release.bat
+++ test/t/torque/commands/qmgr/release.bat
@@ -19,7 +19,7 @@ execute_tests("$testbase/setup.t")
 
 execute_tests(
               "$testbase/qmgr_c_scheduling.t",
-# JR-TRQ-475              "$testbase/qmgr_c_queue.t",
+              "$testbase/qmgr_c_queue.t",
               "$testbase/qmgr_c_server_max_slot_limit.t",
 );
 
