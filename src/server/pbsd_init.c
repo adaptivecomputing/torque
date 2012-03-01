d2fe33d4c9d5cfefb1345923bb28c6be9c34a7c8
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2012-03-01 22:57:56 +0000

    When running without a hierarchy, set the hiearchy level for each node to 0

--- src/server/pbsd_init.c
+++ src/server/pbsd_init.c
@@ -473,6 +473,8 @@ dynamic_string *make_default_hierarchy()
       append_dynamic_string(level_ds, buf);
       }
 
+    pnode->nd_hierarchy_level = 0;
+
     unlock_node(pnode, __func__, NULL, LOGLEVEL);
     }
 
