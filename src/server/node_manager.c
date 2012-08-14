d135aad935fcee00cac3f03a28693e4c88fb91cf
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2012-08-14 15:59:13 +0000

    TRQ-1074. down_on_error wasn't working because the error was processsed before the state string, making it so that the node was marked down and then up again in the same status update.

--- src/server/node_manager.c
+++ src/server/node_manager.c
@@ -1544,6 +1544,7 @@ int process_status_info(
   long            mom_job_sync = FALSE;
   long            auto_np = FALSE;
   long            down_on_error = FALSE;
+  int             dont_change_state = FALSE;
   pbs_attribute   temp;
   int             rc = PBSE_NONE;
   int             send_hello = FALSE;
@@ -1576,6 +1577,8 @@ int process_status_info(
       /* if we've already processed some, save this before moving on */
       if (str != status_info->str)
         save_node_status(current, &temp);
+      
+      dont_change_state = FALSE;
 
       if ((current = get_numa_from_str(str, current)) == NULL)
         break;
@@ -1587,6 +1590,8 @@ int process_status_info(
       /* if we've already processed some, save this before moving on */
       if (str != status_info->str)
         save_node_status(current, &temp);
+
+      dont_change_state = FALSE;
 
       if ((current = get_node_from_str(str, name, current)) == NULL)
         break;
@@ -1622,7 +1627,8 @@ int process_status_info(
 
     if (!strncmp(str, "state", 5))
       {
-      process_state_str(current, str);
+      if (dont_change_state == FALSE)
+        process_state_str(current, str);
       }
     else if ((allow_any_mom == TRUE) &&
              (!strncmp(str, "uname", 5))) 
@@ -1633,7 +1639,10 @@ int process_status_info(
       {
       if ((!strncmp(str, "message=ERROR", 13)) &&
           (down_on_error == TRUE))
+        {
         update_node_state(current, INUSE_DOWN);
+        dont_change_state = TRUE;
+        }
       }
     else if ((mom_job_sync == TRUE) &&
              (!strncmp(str, "jobdata=", 8)))
