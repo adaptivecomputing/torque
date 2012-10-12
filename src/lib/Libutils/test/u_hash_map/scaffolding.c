882c298834cb35189b673e2211dfe501fa71e775
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2012-10-12 21:17:28 +0000

    Add unit testing for the hash map class

--- src/lib/Libutils/test/u_hash_map/scaffolding.c
+++ src/lib/Libutils/test/u_hash_map/scaffolding.c
@@ -1,2 +1,69 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "resizable_array.h"
+#include "hash_table.h"
+
+int insert_thing(resizable_array *ra, void *thing)
+  {
+  if ((int *)thing == (int *)1)
+    {
+    return(-1);
+    }
+  else
+    {
+    return(0);
+    }
+  }
+
+int remove_thing_from_index(resizable_array *ra, int index)
+  {
+  return(0);
+  }
+
+int add_hash(hash_table_t *ht, int value, void *key)
+  {
+  return(0);
+  }
+
+void free_hash(hash_table_t *ht) {}
+
+int get_value_hash(hash_table_t *ht, void *key)
+  {
+  if (!strcmp((char *)key, "tom1"))
+    return(0);
+  else
+    return(-1);
+  }
+
+resizable_array *initialize_resizable_array(int size)
+  {
+  static resizable_array ra;
+  static slot            slots[2];
+  static int             a = 4;
+
+  slots[0].item = &a;
+  ra.slots = slots;
+
+  return(&ra);
+  }
+
+void *next_thing(resizable_array *ra, int *iter)
+  {
+  return(NULL);
+  }
+
+hash_table_t *create_hash(int size)
+  {
+  return(NULL);
+  }
+
+void free_resizable_array(resizable_array *ra) {}
+
+void log_err(int errnum, const char *routine, char *text) {}
+
+int remove_hash(hash_table_t *ht, char *key) 
+  {
+  return(0);
+  }
--- src/lib/Libutils/test/u_hash_map/test_u_hash_map.c
+++ src/lib/Libutils/test/u_hash_map/test_u_hash_map.c
@@ -1,18 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <check.h>
+#include <errno.h>
 
+#include "hash_map.h"
 
-START_TEST(test_one)
+START_TEST(get_hash_map_test)
   {
+  hash_map *hm = get_hash_map(2);
+  fail_unless(hm != NULL, "Couldn't initialize hash map");
   }
 END_TEST
 
 
 
 
-START_TEST(test_two)
+START_TEST(free_hash_map_test)
   {
+  hash_map *hm = get_hash_map(2);
+  fail_unless(hm != NULL, "Couldn't initialize hash map");
+  free_hash_map(hm);
+  }
+END_TEST
+
+
+
+
+START_TEST(add_to_hash_map_test)
+  {
+  int      *obj1 = (int *)1;
+  int      *obj2 = (int *)2;
+  char     *key1 = "tom1";
+  char     *key2 = "tom2";
+  hash_map *hm = get_hash_map(2);
+
+  fail_unless(add_to_hash_map(hm, obj1, key1) == ALREADY_IN_HASH_MAP, "re-added an object to the hash map");
+  fail_unless(add_to_hash_map(hm, obj1, key2) == ENOMEM, "didn't handle OOM condition");
+  fail_unless(add_to_hash_map(hm, obj2, key2) == 0, "failed to add a valid object");
+  }
+END_TEST
+
+
+
+
+START_TEST(remove_from_hash_map_test)
+  {
+  char     *key1 = "tom1";
+  char     *key2 = "tom2";
+  hash_map *hm   = get_hash_map(2);
+
+  fail_unless(remove_from_hash_map(hm, key2) == KEY_NOT_FOUND, "removed non-existent object?");
+  fail_unless(remove_from_hash_map(hm, key1) == 0, "didn't remove object successfully");
+  }
+END_TEST
+
+
+
+
+START_TEST(get_remove_from_hash_map_test)
+  {
+  char     *key1 = "tom1";
+  char     *key2 = "tom2";
+  hash_map *hm   = get_hash_map(2);
+  
+  fail_unless(get_remove_from_hash_map(hm, key2) == NULL, "removed non-existent object?");
+  fail_unless(get_remove_from_hash_map(hm, key1) != NULL, "didn't remove object successfully");
+  }
+END_TEST
+
+
+
+
+START_TEST(get_from_hash_map_test)
+  {
+  char     *key1 = "tom1";
+  char     *key2 = "tom2";
+  hash_map *hm   = get_hash_map(2);
+  
+  fail_unless(get_from_hash_map(hm, key2) == NULL, "got non-existent object?");
+  fail_unless(get_from_hash_map(hm, key1) != NULL, "didn't get object successfully");
+  }
+END_TEST
+
+
+
+
+START_TEST(next_from_hash_map_test)
+  {
+  hash_map *hm   = get_hash_map(-1);
+  int       iter = -1;
+
+  fail_unless(next_from_hash_map(hm, &iter) == NULL, "iterated from empty hash map?");
   }
 END_TEST
 
@@ -22,12 +100,32 @@ END_TEST
 Suite *u_hash_map_suite(void)
   {
   Suite *s = suite_create("u_hash_map test suite methods");
-  TCase *tc_core = tcase_create("test_one");
-  tcase_add_test(tc_core, test_one);
+  TCase *tc_core = tcase_create("get_hash_map_test");
+  tcase_add_test(tc_core, get_hash_map_test);
   suite_add_tcase(s, tc_core);
   
-  tc_core = tcase_create("test_two");
-  tcase_add_test(tc_core, test_two);
+  tc_core = tcase_create("free_hash_map_test");
+  tcase_add_test(tc_core, free_hash_map_test);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("add_to_hash_map_test");
+  tcase_add_test(tc_core, add_to_hash_map_test);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("remove_from_hash_map_test");
+  tcase_add_test(tc_core, remove_from_hash_map_test);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("get_remove_from_hash_map_test");
+  tcase_add_test(tc_core, get_remove_from_hash_map_test);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("get_from_hash_map_test");
+  tcase_add_test(tc_core, get_from_hash_map_test);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("next_from_hash_map_test");
+  tcase_add_test(tc_core, next_from_hash_map_test);
   suite_add_tcase(s, tc_core);
   
   return(s);
