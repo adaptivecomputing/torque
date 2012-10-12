bef468c64ccca5f6631af5807242ec6798ec3009
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2012-10-12 18:26:48 +0000

    Add unit tests for the exiting jobs code.

--- src/server/test/exiting_jobs/scaffolding.c
+++ src/server/test/exiting_jobs/scaffolding.c
@@ -3,20 +3,25 @@
 
 #include "hash_map.h"
 #include "pbs_job.h"
+#include "exiting_jobs.h"
+#include "batch_request.h"
+#include "hash_table.h"
+
+int LOGLEVEL;
+hash_map *exiting_jobs_info;
 
 int add_to_hash_map(hash_map *hm, void *obj, char *key)
   {
-  return(0);
+  if (!strcmp(key, "1.tom"))
+    return(0);
+  else
+    return(-1);
   }
 
 void *get_remove_from_hash_map(hash_map *hm, char *key)
   {
-  static job pjob;
-
-  strcpy(pjob->ji_qs.ji_jobid, "1.napali");
-
-  if (!strcmp(key, pjob->ji_qs.ji_jobid))
-    return(&pjob);
+  if (!strcmp(key, "2.napali"))
+    return(calloc(1, sizeof(job_exiting_retry_info)));
   else
     return(NULL);
   }
@@ -24,17 +29,27 @@ void *get_remove_from_hash_map(hash_map *hm, char *key)
 
 int remove_from_hash_map(hash_map *hm, char *key)
   {
-  return(PBSE_NONE);
+  if (!strcmp(key, "3.napali"))
+    return(PBSE_NONE);
+  else
+    return(KEY_NOT_FOUND);
   }
 
 job *svr_find_job(char *jobid, int a)
   {
   static job pjob;
 
-  strcpy(pjob->ji_qs.ji_jobid, "1.napali");
-
   if (!strcmp(jobid, "1.napali"))
+    {
+    strcpy(pjob.ji_qs.ji_jobid, "1.napali");
     return(&pjob);
+    }
+  else if (!strcmp(jobid, "5.napali"))
+    {
+    strcpy(pjob.ji_qs.ji_jobid, "5.napali");
+    pjob.ji_qs.ji_state = JOB_STATE_COMPLETE;
+    return(&pjob);
+    }
   else
     return(NULL);
   }
@@ -42,13 +57,21 @@ job *svr_find_job(char *jobid, int a)
 
 void *next_from_hash_map(hash_map *hm, int *iter)
   {
-  static job_exiting_retry_info jeri;
-
+  job_exiting_retry_info *jeri;
+    
   if (*iter < 2)
     {
-    snprintf(jeri->jobid, sizeof(jeri->jobid), "%d.napali", 10+*iter);
-    *iter++;
-    return(&jeri);
+    jeri = calloc(1, sizeof(job_exiting_retry_info));
+
+    if (*iter == -1)
+      snprintf(jeri->jobid, sizeof(jeri->jobid), "1.napali");
+    else if (*iter == 0)
+      snprintf(jeri->jobid, sizeof(jeri->jobid), "5.napali");
+    else if (*iter == 1)
+      snprintf(jeri->jobid, sizeof(jeri->jobid), "%d.napali", 10+*iter);
+
+    (*iter)++;
+    return(jeri);
     }
   else
     return(NULL);
@@ -59,3 +82,8 @@ int unlock_ji_mutex(job *pjob, const char *id, char *msg, int logging)
   return(0);
   }
 
+void on_job_exit(batch_request *preq, char *jobid) {}
+
+void force_purge_work(job *pjob) {}
+
+void log_event(int eventtype, int objclass, const char *objname, char *text) {}
--- src/server/test/exiting_jobs/test_exiting_jobs.c
+++ src/server/test/exiting_jobs/test_exiting_jobs.c
@@ -2,17 +2,82 @@
 #include <stdlib.h>
 #include <check.h>
 
+#include "exiting_jobs.h"
+#include "log.h"
+#include "hash_table.h"
 
-START_TEST(test_one)
+int remove_entry_from_exiting_list(job_exiting_retry_info *jeri);
+int retry_job_exit(job_exiting_retry_info *jeri);
+int check_exiting_jobs();
+
+
+START_TEST(record_job_as_exiting_test)
   {
+  job pjob;
+
+  memset(&pjob, 0, sizeof(pjob));
+  strcpy(pjob.ji_qs.ji_jobid, "1.tom");
+
+  fail_unless(record_job_as_exiting(&pjob) == 0, "Couldn't record job as exiting");
   }
 END_TEST
 
 
 
 
-START_TEST(test_two)
+START_TEST(remove_job_from_exiting_list_test)
   {
+  job pjob;
+
+  memset(&pjob, 0, sizeof(pjob));
+
+  strcpy(pjob.ji_qs.ji_jobid, "2.napali");
+  fail_unless(remove_job_from_exiting_list(&pjob) == 0, "Couldn't remove job from exiting list");
+ 
+  strcpy(pjob.ji_qs.ji_jobid, "1.napali");
+  fail_unless(remove_job_from_exiting_list(&pjob) == 0, "Couldn't remove job from exiting list");
+  }
+END_TEST
+
+
+
+
+START_TEST(remove_entry_from_exiting_list_test)
+  {
+  job_exiting_retry_info *jeri = calloc(1, sizeof(job_exiting_retry_info));
+  strcpy(jeri->jobid, "3.napali");
+  fail_unless(remove_entry_from_exiting_list(jeri) == 0, "Couldn't remove entry");
+  
+  jeri = calloc(1, sizeof(job_exiting_retry_info));
+  strcpy(jeri->jobid, "1.napali");
+  fail_unless(remove_entry_from_exiting_list(jeri) == KEY_NOT_FOUND, "Removed entry that wasn't there");
+
+  }
+END_TEST
+
+
+
+
+START_TEST(retry_job_exit_test)
+  {
+  job_exiting_retry_info *jeri = calloc(1, sizeof(job_exiting_retry_info));
+
+  strcpy(jeri->jobid, "1.napali");
+
+  fail_unless(retry_job_exit(jeri) == 0, "Didn't retry job");
+  fail_unless(jeri->attempts == 1, "Didn't increment attempt count");
+
+  jeri->attempts = 100;
+  fail_unless(retry_job_exit(jeri) == 0, "Didn't remove after max attempts");
+  }
+END_TEST
+
+
+
+
+START_TEST(check_exiting_jobs_test)
+  {
+  fail_unless(check_exiting_jobs() == 0, "Failure when inspecting");
   }
 END_TEST
 
@@ -22,12 +87,24 @@ END_TEST
 Suite *exiting_jobs_suite(void)
   {
   Suite *s = suite_create("exiting_jobs test suite methods");
-  TCase *tc_core = tcase_create("test_one");
-  tcase_add_test(tc_core, test_one);
+  TCase *tc_core = tcase_create("record_job_as_exiting_test");
+  tcase_add_test(tc_core, record_job_as_exiting_test);
   suite_add_tcase(s, tc_core);
   
-  tc_core = tcase_create("test_two");
-  tcase_add_test(tc_core, test_two);
+  tc_core = tcase_create("remove_job_from_exiting_list_test");
+  tcase_add_test(tc_core, remove_job_from_exiting_list_test);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("remove_entry_from_exiting_list_test");
+  tcase_add_test(tc_core, remove_entry_from_exiting_list_test);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("retry_job_exit_test");
+  tcase_add_test(tc_core, retry_job_exit_test);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("check_exiting_jobs_test");
+  tcase_add_test(tc_core, check_exiting_jobs_test);
   suite_add_tcase(s, tc_core);
   
   return(s);
