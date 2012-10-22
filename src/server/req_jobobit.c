927cee35458339f8bf50bdf3ae92c045adb6d7aa
echan <echan@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2012-10-22 19:00:14 +0000

    echan: merged changelist r6968 from 4.1-fixes so compiling using higher versions of gcc won't abort because of 'variable set but not used' when the option of --enable-gg-warnings was used.

--- src/server/req_jobobit.c
+++ src/server/req_jobobit.c
@@ -3071,7 +3071,11 @@ int req_jobobit(
     
     free_br(tmppreq);
     }
-
+#else
+   if (!have_resc_used) 
+     {
+       /* this is so higher versions of gcc won't abort because of the condition 'variable set but not used' */
+     }
 #endif    /* USESAVEDRESOURCES */
 
   safe_strncat(mailbuf, (acctbuf + accttail), sizeof(mailbuf) - strlen(mailbuf) - 1);
