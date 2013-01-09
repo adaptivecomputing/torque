99eaa6306e4bb391e2a16b5b10dfc253b056e36d
bdaw <bdaw@adaptivecomputing.com>
2013-01-09 11:48:29 -0700

    Adding files to start the process of updating the unit test framework.

--- src/lib/Libattr/test/attr_func/scaffolding.c
+++ src/lib/Libattr/test/attr_func/scaffolding.c
@@ -1,8 +1,20 @@
-#include "license_pbs.h" /* See here for the software license */
+  #include "license_pbs.h" /* See here for the software license */
 #include <stdlib.h>
 #include <stdio.h>
 
+#define banana \
+	avian_tuber
+
+//Hey, a comment
+
 #include "list_link.h" /* list_link */
+
+void im_dying();
+
+void im_dying()
+  {
+    fprintf(stderr,"I'm in %s and I'm not dying.\n",__FILE__);
+  }
 
 void *get_next(list_link pl, char *file, int line)
   {
