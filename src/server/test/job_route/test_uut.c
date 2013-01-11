5309e17e5d911400bca30d0f3774a2b6a1e30915
bdaw <bdaw@adaptivecomputing.com>
2013-01-11 11:08:03 -0700

    Moved job_route to new style unit testing.

---
+++ src/server/test/job_route/test_uut.c
@@ -0,0 +1,73 @@
+#include "license_pbs.h" /* See here for the software license */
+#include "job_route.h"
+#include "test_uut.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "pbs_error.h"
+#include "mutex_mgr.hpp"
+
+
+START_TEST(test_add_dest_null)
+  {
+  /* This used to cause a seg fault. If it executes without
+     crashing, the test passes. */
+  add_dest(NULL); 
+  }
+END_TEST
+
+START_TEST(test_add_dest)
+  {
+  struct job j;
+  CLEAR_HEAD(j.ji_rejectdest);
+  strcpy(j.ji_qs.ji_destin, "test");
+  add_dest(&j);
+  }
+END_TEST
+
+START_TEST(test_queue_route_null)
+  {
+  fail_unless(NULL == queue_route(NULL), "expected queue_route(NULL) to return NULL");
+  }
+END_TEST
+
+START_TEST(test_is_bad_dest)
+  {
+  struct job j;
+  CLEAR_HEAD(j.ji_rejectdest);
+  strcpy(j.ji_qs.ji_destin, "test");
+  add_dest(&j);
+  fail_unless(NULL == is_bad_dest(&j, (char *)"random"));
+  fail_unless(NULL != is_bad_dest(&j, (char *)"test"));
+  }
+END_TEST
+
+Suite *job_route_suite(void)
+  {
+  Suite *s = suite_create("job_route_suite methods");
+  TCase * tc = tcase_create("job_route");
+
+  tcase_add_test(tc, test_add_dest_null);
+  tcase_add_test(tc, test_add_dest);
+  tcase_add_test(tc, test_queue_route_null);
+  tcase_add_test(tc, test_is_bad_dest);
+  
+  suite_add_tcase(s, tc);
+  return s;
+  }
+
+void rundebug()
+  {
+  }
+
+int main(void)
+  {
+  int number_failed = 0;
+  SRunner *sr = NULL;
+  rundebug();
+  sr = srunner_create(job_route_suite());
+  srunner_set_log(sr, "job_route_suite.log");
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return number_failed;
+  }
