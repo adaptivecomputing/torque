803c8de90af50a2d5b06d1ce9f0a683af5f18ee1
Ken <knielson@adaptivecomputing.com>
2013-01-16 14:03:28 -0700

    Added mutex_mgr to job in process_request()

--- src/server/process_request.c
+++ src/server/process_request.c
@@ -147,6 +147,7 @@
 #include "job_func.h" /* svr_job_purge */
 #include "tcp.h" /* tcp_chan */
 #include "ji_mutex.h"
+#include "mutex_mgr.hpp"
 
 /*
  * process_request - this function gets, checks, and invokes the proper
@@ -598,8 +599,9 @@ int process_request(
         *dptr = '\0';
         }
       
-      if ((pjob = svr_find_job(request->rq_ind.rq_modify.rq_objname, FALSE)) != (job *)0)
+      if ((pjob = svr_find_job(request->rq_ind.rq_modify.rq_objname, FALSE)) != NULL)
         {
+        mutex_mgr pjob_mutex = mutex_mgr(pjob->ji_mutex, true);
         if (pjob->ji_qs.ji_state == JOB_STATE_RUNNING)
           {
 
@@ -612,9 +614,7 @@ int process_request(
             request->rq_perm = svr_get_privilege(request->rq_user, server_host);
             skip = TRUE;
             }
-
           }
-        unlock_ji_mutex(pjob, __func__, "1", LOGLEVEL);
         }
       
       if (!skip)
