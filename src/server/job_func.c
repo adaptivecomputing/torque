1c5fb2a80fd4075c1ebfb722dc6f80b1fe692e93
David Beer <dbeer@adaptivecomputing.com>
2013-01-23 10:31:34 -0700

    Fix a crash in get_jobs_array(), if the job has disappeared, then we need to make sure we have a valid array before attempting to unlock it.

--- src/server/job_func.c
+++ src/server/job_func.c
@@ -2056,8 +2056,12 @@ job_array *get_jobs_array(
     pjob = svr_find_job(jobid, TRUE);
     if (pjob == NULL)
       {
-      unlock_ai_mutex(pa, __func__, "1", LOGLEVEL);
-      pa = NULL;
+      if (pa != NULL)
+        {
+        unlock_ai_mutex(pa, __func__, "1", LOGLEVEL);
+        pa = NULL;
+        }
+
       *pjob_ptr = NULL;
       }
     mutex_mgr job_mutex2(pjob->ji_mutex,true);
