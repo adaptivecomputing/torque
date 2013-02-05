ad86f93e157d68240b2acd17bff079cd12cf2c72
David Beer <dbeer@adaptivecomputing.com>
2013-02-05 11:29:37 -0700

    In copy_properties(), make sure we iterate over all of the properties in the array strings struct.

--- src/server/node_func.c
+++ src/server/node_func.c
@@ -1448,7 +1448,7 @@ int copy_properties(
   main_node = src->nd_prop;
  
   /* allocate the properties for the numa node */
-  need = sizeof(struct array_strings) + main_node->as_npointers - 1;
+  need = sizeof(struct array_strings) + ((main_node->as_npointers - 1) * sizeof(char *));
   dest->nd_prop = (struct array_strings *)calloc(1, need);
   sub  = dest->nd_prop;
 
@@ -1468,7 +1468,7 @@ int copy_properties(
 
   plink = &dest->nd_first;
 
-  for (i = 0; i < main_node->as_npointers - 1; i++)
+  for (i = 0; i < main_node->as_npointers; i++)
     {
     sub->as_string[i] = sub->as_buf + (main_node->as_string[i] - main_node->as_buf);
 
