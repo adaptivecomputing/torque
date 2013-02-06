6cda8f598e3642a8f3b95a59a6e75109f35699a0
Ken <knielson@adaptivecomputing.com>
2013-02-06 12:11:36 -0700

    Added unit test for decode_tv

--- src/lib/Libattr/test/attr_fn_tv/test_attr_fn_tv.c
+++ src/lib/Libattr/test/attr_fn_tv/test_attr_fn_tv.c
@@ -7,9 +7,133 @@
 
 #include "pbs_error.h"
 
-START_TEST(test_one)
+START_TEST(decode_tv)
   {
+  pbs_attribute pattr;
+  char    name[32];
+  char    rescn[32];
+  char    val[32];
+  char    EMsg[48];
+  int     perm = 0;
+  int     rc;
+  
+  memset(&pattr, 0, sizeof(pbs_attribute));
+  strcpy(name, "freddy");
+  strcpy(rescn, "someresc");
+  strcpy(val, "33:3");
 
+  rc = decode_tv(&pattr, name, rescn, val, perm);
+  sprintf(EMsg, "decode_tv unexpectedly failed: %d", rc);
+  fail_unless(rc == PBSE_NONE, EMsg);
+
+  }
+END_TEST
+
+START_TEST(decode_tv_no_colon)
+  {
+  pbs_attribute pattr;
+  char    name[32];
+  char    rescn[32];
+  char    val[32];
+  char    EMsg[48];
+  int     perm = 0;
+  int     rc;
+  
+  memset(&pattr, 0, sizeof(pbs_attribute));
+  strcpy(name, "freddy");
+  strcpy(rescn, "someresc");
+  strcpy(val, "33");
+
+  rc = decode_tv(&pattr, name, rescn, val, perm);
+  sprintf(EMsg, "decode_tv unexpectedly failed: %d", rc);
+  fail_unless(rc == PBSE_NONE, EMsg);
+
+  }
+END_TEST
+
+START_TEST(decode_tv_two_colons)
+  {
+  pbs_attribute pattr;
+  char    name[32];
+  char    rescn[32];
+  char    val[32];
+  char    EMsg[48];
+  int     perm = 0;
+  int     rc;
+  
+  memset(&pattr, 0, sizeof(pbs_attribute));
+  strcpy(name, "freddy");
+  strcpy(rescn, "someresc");
+  strcpy(val, "33:44:55");
+
+  rc = decode_tv(&pattr, name, rescn, val, perm);
+  sprintf(EMsg, "decode_tv unexpectedly failed: %d", rc);
+  fail_unless(rc == PBSE_BADATVAL, EMsg);
+
+  }
+END_TEST
+
+START_TEST(decode_tv_not_digit)
+  {
+  pbs_attribute pattr;
+  char    name[32];
+  char    rescn[32];
+  char    val[32];
+  char    EMsg[48];
+  int     perm = 0;
+  int     rc;
+  
+  memset(&pattr, 0, sizeof(pbs_attribute));
+  strcpy(name, "freddy");
+  strcpy(rescn, "someresc");
+  strcpy(val, "XX:44");
+
+  rc = decode_tv(&pattr, name, rescn, val, perm);
+  sprintf(EMsg, "decode_tv unexpectedly failed: %d", rc);
+  fail_unless(rc == PBSE_BADATVAL, EMsg);
+
+  }
+END_TEST
+
+START_TEST(decode_tv_null_val)
+  {
+  pbs_attribute pattr;
+  char    name[32];
+  char    rescn[32];
+  char    *val = NULL;
+  char    EMsg[48];
+  int     perm = 0;
+  int     rc;
+  
+  memset(&pattr, 0, sizeof(pbs_attribute));
+  strcpy(name, "freddy");
+  strcpy(rescn, "someresc");
+
+  rc = decode_tv(&pattr, name, rescn, val, perm);
+  sprintf(EMsg, "decode_tv unexpectedly failed: %d", rc);
+  fail_unless(rc == PBSE_NONE, EMsg);
+
+  }
+END_TEST
+
+START_TEST(decode_tv_val_empty)
+  {
+  pbs_attribute pattr;
+  char    name[32];
+  char    rescn[32];
+  char    val[32];
+  char    EMsg[48];
+  int     perm = 0;
+  int     rc;
+  
+  memset(&pattr, 0, sizeof(pbs_attribute));
+  memset(val, 0, 32);
+  strcpy(name, "freddy");
+  strcpy(rescn, "someresc");
+
+  rc = decode_tv(&pattr, name, rescn, val, perm);
+  sprintf(EMsg, "decode_tv unexpectedly failed: %d", rc);
+  fail_unless(rc == PBSE_NONE, EMsg);
 
   }
 END_TEST
@@ -21,11 +145,32 @@ START_TEST(test_two)
   }
 END_TEST
 
+
 Suite *attr_fn_tv_suite(void)
   {
   Suite *s = suite_create("attr_fn_tv_suite methods");
-  TCase *tc_core = tcase_create("test_one");
-  tcase_add_test(tc_core, test_one);
+  TCase *tc_core = tcase_create("decode_tv");
+  tcase_add_test(tc_core, decode_tv);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("decode_tv_no_colon");
+  tcase_add_test(tc_core, decode_tv_no_colon);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("decode_tv_two_colons");
+  tcase_add_test(tc_core, decode_tv_two_colons);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("decode_tv_not_digit");
+  tcase_add_test(tc_core, decode_tv_not_digit);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("decode_tv_null_val");
+  tcase_add_test(tc_core, decode_tv_null_val);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("decode_tv_val_empty");
+  tcase_add_test(tc_core, decode_tv_val_empty);
   suite_add_tcase(s, tc_core);
 
   tc_core = tcase_create("test_two");
