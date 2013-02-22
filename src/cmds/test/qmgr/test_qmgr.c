3adc5cf9297f76e2329cafe4fdd46387a50b5437
Elizabeth Chan <echan@adaptivecomputing.com>
2013-02-22 15:32:58 -0700

    Merged changes for TRQ-897: TORQUE rejecting jobs because of resources_max.nodes when it shouldn't.

--- src/cmds/test/qmgr/test_qmgr.c
+++ src/cmds/test/qmgr/test_qmgr.c
@@ -1,35 +1,194 @@
 #include "license_pbs.h" /* See here for the software license */
 #include "qmgr_functions.h"
+#include "qmgr.h"
 #include "test_qmgr.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 
 #include "pbs_error.h"
+void free_attrib(
 
-START_TEST(test_one)
+  struct attropl **attribute)
+
   {
+    struct attropl *p = *attribute; 
+    if (p->name)
+      free(p->name);
 
+    if (p->value)
+      free(p->value);
 
+    if (p->resource)
+      free(p->resource);
+    
+    free(p);
+    *attribute = NULL;
+  }
+
+START_TEST(test_res_max_nodes)
+  {
+  const char *copt = "s q one_nodeq resources_max.nodes = 1:ppn=32";
+  int type, oper, errflg;
+  char *name = NULL;
+  struct attropl *attribs = NULL;
+
+  type = oper = 0;
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(4 == errflg, "fail to detect resource_max.nodes incorrect syntax");
+
+  name = NULL;
+  if (attribs)
+    {
+    free_attrib(&attribs);
+    }
+  oper = type = 0;
+  copt = "s q one_nodeq resources_max.nodes = 5";
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(0 == errflg, "fail to detect resource_max.nodes correct syntax");
   }
 END_TEST
 
-START_TEST(test_two)
+START_TEST(test_res_max_nodect)
   {
+  const char *copt = "s q one_nodeq resources_max.nodect = 1:ppn=32";
+  int type, oper, errflg; 
+  char *name = NULL;
+  struct attropl *attribs = NULL;
 
+  type = oper = 0;
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(4 == errflg, "fail to detect resource_max.nodect incorrect syntax");
 
+  name = NULL;
+  if (attribs)
+    {
+    free_attrib(&attribs);
+    }
+  oper = type = 0;
+  copt = "s q one_nodeq resources_max.nodect = 5";
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(0 == errflg, "fail to detect resouces_max.nodect correct syntax");
+  }
+END_TEST
+
+START_TEST(test_res_max_procct)
+  {
+  const char *copt = "s q one_nodeq resources_max.procct = 1:ppn=32";
+  int type, oper, errflg; 
+  char *name = NULL;
+  struct attropl *attribs = NULL;
+
+  type = oper = 0;
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(4 == errflg, "fail to detect resource_max.procct incorrect syntax");
+
+  name = NULL;
+  if (attribs)
+    {
+    free_attrib(&attribs);
+    }
+  oper = type = 0;
+  copt = "s q one_nodeq resources_max.procct = 5";
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(0 == errflg, "fail to detect resource_max.procct correct syntax");
+  }
+END_TEST
+
+START_TEST(test_res_min_nodes)
+  {
+  const char *copt = "s q one_nodeq resources_min.nodes = 1:ppn=32";
+  int type, oper, errflg; 
+  char *name = NULL;
+  struct attropl *attribs = NULL;
+
+  type = oper = 0;
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(4 == errflg, "fail to detect resource_min.nodes incorrect syntax");
+
+  name = NULL;
+  if (attribs)
+    {
+    free_attrib(&attribs);
+    }
+  oper = type = 0;
+  copt = "s q one_nodeq resources_min.nodes = 5";
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(0 == errflg, "fail to detect resource_min.nodes correct syntax");
+  }
+END_TEST
+
+START_TEST(test_res_min_nodect)
+  {
+  const char *copt = "s q one_nodeq resources_min.nodect = 1:ppn=32";
+  int type, oper, errflg; 
+  char *name = NULL;
+  struct attropl *attribs = NULL;
+
+  type = oper = 0;
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(4 == errflg, "fail to detect resource_min.nodect incorrect syntax");
+
+  name = NULL;
+  if (attribs)
+    {
+    free_attrib(&attribs);
+    }
+  oper = type = 0;
+  copt = "s q one_nodeq resources_min.nodes = 5";
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(0 == errflg, "fail to detect resource_min.nodect correct syntax");
+  }
+END_TEST
+
+START_TEST(test_res_min_procct)
+  {
+  const char *copt = "s q one_nodeq resources_min.procct = 1:ppn=32";
+  int type, oper, errflg; 
+  char *name = NULL;
+  struct attropl *attribs = NULL;
+
+  type = oper = 0;
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(4 == errflg, "fail to detect resource_min.procct incorrect syntax");
+
+  name = NULL;
+  if (attribs)
+    {
+    free_attrib(&attribs);
+    }
+  oper = type = 0;
+  copt = "s q one_nodeq resources_min.procct = 5";
+  errflg = parse((char *)copt, &oper, &type, &name, &attribs);
+  fail_unless(0 == errflg, "fail to detect resouce_min.procct correct syntax");
   }
 END_TEST
 
 Suite *qmgr_suite(void)
   {
   Suite *s = suite_create("qmgr_suite methods");
-  TCase *tc_core = tcase_create("test_one");
-  tcase_add_test(tc_core, test_one);
+  TCase *tc_core = tcase_create("test_res_max_nodes");
+  tcase_add_test(tc_core, test_res_max_nodes);
   suite_add_tcase(s, tc_core);
 
-  tc_core = tcase_create("test_two");
-  tcase_add_test(tc_core, test_two);
+  tc_core = tcase_create("test_res_max_nodect");
+  tcase_add_test(tc_core, test_res_max_nodect);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_res_max_procct");
+  tcase_add_test(tc_core, test_res_max_procct);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_res_min_nodes");
+  tcase_add_test(tc_core, test_res_min_nodes);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_res_min_nodect");
+  tcase_add_test(tc_core, test_res_min_nodect);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_res_min_procct");
+  tcase_add_test(tc_core, test_res_min_procct);
   suite_add_tcase(s, tc_core);
 
   return s;
