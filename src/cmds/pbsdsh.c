07266942d37d322edb9ff0f06b626594c45343f7
Elizabeth Chan <echan@adaptivecomputing.com>
2013-03-26 18:42:42 -0600

    Merged from the 4.1-dev branch - trq-1484 - pbsdsh line of code with better logging.

--- src/cmds/pbsdsh.c
+++ src/cmds/pbsdsh.c
@@ -697,7 +697,7 @@ int main(
   int rc;
 
   int  nspawned = 0;
-  tm_node_id *nodelist;
+  tm_node_id *nodelist = NULL;
   int start;
   int stop;
   int sync = 0;
@@ -901,10 +901,12 @@ int main(
 
   if ((rc = tm_nodeinfo(&nodelist, &numnodes)) != TM_SUCCESS)
     {
-    fprintf(stderr, "%s: tm_nodeinfo failed, rc = %s (%d)\n",
+   fprintf(stderr, "%s: tm_nodeinfo failed, rc = %s (%d) nodelist= %d numnodes= %d\n",
       id,
       get_ecname(rc),
-      rc);
+      rc,
+      (nodelist==NULL) ? -1 : *nodelist,
+      numnodes);
 
     return(1);
     }
