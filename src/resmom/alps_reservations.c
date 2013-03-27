ddf3f22c16bfdc86cf52be675138b03d084a96af
David Beer <dbeer@adaptivecomputing.com>
2013-03-27 17:20:35 -0600

    TRQ-1660. nppn wasn't being passed to alps reservations because a parameter was being mixed up in the alps request. mppwidth and use_nppn were getting switched, so things weren't working.

--- src/resmom/alps_reservations.c
+++ src/resmom/alps_reservations.c
@@ -307,8 +307,8 @@ dynamic_string *get_reservation_command(
   char            *apbasil_path,
   char            *apbasil_protocol,
   char            *multi_req_list,
-  int              mppdepth,
-  int              use_nppn)
+  int              use_nppn,
+  int              mppdepth)
 
   {
   dynamic_string *command = get_dynamic_string(-1, NULL);
