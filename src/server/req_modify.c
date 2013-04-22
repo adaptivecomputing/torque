6caa47164b9e3f1ce6db25e200a1eda45c9d2400
David Beer <dbeer@adaptivecomputing.com>
2013-04-22 11:42:36 -0600

    Fix some errors in the changes to how qalter is processed. Namely, account for the fact that modify_job_work() also calls modify_job().

--- src/server/req_modify.c
+++ src/server/req_modify.c
@@ -419,13 +419,12 @@ int modify_job(
     sprintf(log_buf, "job structure is NULL");
     log_err(PBSE_IVALREQ, __func__, log_buf);
 
-    free_br(preq);
+    req_reject(PBSE_IVALREQ, 0, preq, NULL, NULL);
 
     return(PBSE_IVALREQ);
     }
 
   /* cannot be in exiting or transit, exiting has already been checked */
-
   if (pjob->ji_qs.ji_state == JOB_STATE_TRANSIT)
     {
     /* FAILURE */
@@ -435,7 +434,7 @@ int modify_job(
 
     log_err(PBSE_BADSTATE, __func__, log_buf);
     
-    free_br(preq);
+    req_reject(PBSE_BADSTATE, 0, preq, NULL, NULL);
 
     return(PBSE_BADSTATE);
     }
@@ -489,7 +488,7 @@ int modify_job(
           plist->al_name);
         log_err(PBSE_MODATRRUN, __func__, log_buf);
     
-        free_br(preq);
+        reply_badattr(PBSE_MODATRRUN, 1, plist, preq);
 
         return(PBSE_MODATRRUN);
         }
@@ -512,7 +511,7 @@ int modify_job(
 
           log_err(PBSE_UNKRESC, __func__, log_buf);
     
-          free_br(preq);
+          reply_badattr(PBSE_UNKRESC, 1, plist, preq);
 
           return(PBSE_UNKRESC);
           }
@@ -525,7 +524,7 @@ int modify_job(
             plist->al_name);
           log_err(PBSE_MODATRRUN, __func__, log_buf);
     
-          free_br(preq);
+          reply_badattr(PBSE_MODATRRUN, 1, plist, preq);
 
           return(PBSE_MODATRRUN);
           }
@@ -555,7 +554,7 @@ int modify_job(
     if (rc == PBSE_JOBNOTFOUND)
       *j = NULL;
     
-    free_br(preq);
+    req_reject(rc, 0, preq, NULL, NULL);
 
     return(rc);
     }
@@ -625,7 +624,7 @@ int modify_job(
     return(PBSE_RELAYED_TO_MOM);
     }
   else
-    free_br(preq);
+    reply_ack(preq);
 
   if (copy_checkpoint_files)
     {
@@ -644,6 +643,8 @@ int modify_job(
       if (rc != PBSE_NONE)
         {
         free_br(momreq);
+    
+        req_reject(rc, 0, preq, NULL, NULL);
 
         if (pjob != NULL)
           {
@@ -907,7 +908,6 @@ void *modify_job_work(
   {
   job           *pjob;
   svrattrl      *plist;
-  int            rc;
   int            checkpoint_req = FALSE;
   batch_request *preq = (struct batch_request *)vp;
   
@@ -936,22 +936,8 @@ void *modify_job_work(
 
   plist = (svrattrl *)GET_NEXT(preq->rq_ind.rq_modify.rq_attr);
 
-  if ((rc = modify_job((void **)&pjob, plist, preq, checkpoint_req, 0)) != 0)
-    {
-    if ((rc == PBSE_MODATRRUN) ||
-        (rc == PBSE_UNKRESC))
-      {
-      reply_badattr(rc,1,plist,preq);
-      }
-    else if ( rc == PBSE_RELAYED_TO_MOM )
-      {
-      return(NULL);
-      }
-    else
-      req_reject(rc,0,preq,NULL,NULL);
-    }
-  else
-    reply_ack(preq);
+  /* modify_job will free preq and respond to it */
+  modify_job((void **)&pjob, plist, preq, checkpoint_req, 0);
 
   return(NULL);
   } /* END modify_job_work() */
