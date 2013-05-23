07991c8e698393b0f2ac9656d9d8aec0ae6998b7
Elizabeth Chan <echan@adaptivecomputing.com>
2013-05-23 14:25:12 -0600

    Fix for TRQ-1852 - pbs_server 2.5.12 free_br crash.

--- src/server/reply_send.c
+++ src/server/reply_send.c
@@ -302,7 +302,10 @@ int reply_send(
     }
 
 #ifndef PBS_MOM
-  if ((request->rq_type != PBS_BATCH_AsyModifyJob) || (request->rq_noreply == TRUE))
+  if ((request->rq_type != PBS_BATCH_AsyModifyJob) &&
+      (request->rq_type != PBS_BATCH_AsyrunJob) &&
+      (request->rq_type != PBS_BATCH_AsySignalJob)) ||
+      (request->rq_noreply == TRUE))
     {
 #endif
     free_br(request);
