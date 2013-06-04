f53f36fb2661438ade199419c2154783174ab2f6
David Beer <dbeer@adaptivecomputing.com>
2013-06-04 11:02:59 -0600

    TRQ-1830. Don't fall through after finding a permanent failure in socket_connect_addr()

--- src/lib/Libnet/net_common.c
+++ src/lib/Libnet/net_common.c
@@ -365,7 +365,12 @@ int socket_connect_addr(
           {
           close(*local_socket);
           *local_socket = rc;
+
+          /* do not fall through here */
+          break;
           }
+
+        /* essentially, only transient failures fall through */
 
       /* socket not ready for writing after 5 timeout */
       case EINVAL:    /* Invalid argument */
