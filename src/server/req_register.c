892592e5fde5255f52f5e8b3cfdac886f87395f3
David Beer <dbeer@adaptivecomputing.com>
2013-06-06 11:02:12 -0600

    Fix a way in how we determine if a request was local or remote - we were string comparing the hostnames, change this to resolve the hostnames to their IP address and then compare that to be more foolproof. TRQ-1946.

--- src/server/req_register.c
+++ src/server/req_register.c
@@ -2282,6 +2282,9 @@ int send_depend_req(
 
   struct batch_request *preq;
   char                  log_buf[LOCAL_LOG_BUF_SIZE];
+  pbs_net_t             svraddr1;
+  pbs_net_t             svraddr2;
+  int                   my_err;
 
   preq = alloc_br(PBS_BATCH_RegistDep);
 
@@ -2350,9 +2353,14 @@ int send_depend_req(
   get_batch_request_id(preq);
   snprintf(br_id, sizeof(br_id), "%s", preq->rq_id);
 
+  svraddr1 = get_hostaddr(&my_err, server_name);
+  svraddr2 = get_hostaddr(&my_err, pparent->dc_svr);
+
   if ((rc = issue_to_svr(pparent->dc_svr, preq, NULL)) != PBSE_NONE)
     {
-    if (strcmp(pparent->dc_svr, server_name))
+    /* local requests have already been processed and freed. Do not attempt to
+     * free or reference again. */
+    if (svraddr1 != svraddr2)
       {
       free_br(preq);
       }
@@ -2370,7 +2378,9 @@ int send_depend_req(
 
     return(rc);
     }
-  else if (strcmp(pparent->dc_svr, server_name))
+  /* local requests have already been processed and freed. Do not attempt to
+   * free or reference again. */
+  else if (svraddr1 != svraddr2)
     postfunc(preq);
 
   if ((pjob = svr_find_job(job_id, TRUE)) == NULL)
