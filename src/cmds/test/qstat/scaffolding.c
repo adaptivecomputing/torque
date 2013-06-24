4540f3fb6a0581ed5220efd121c4cf7f27b178d9
David Beer <dbeer@adaptivecomputing.com>
2013-06-24 16:51:47 -0600

    TRQ-2006. Fix the elapsed time displaying 00 for hours in all cases.

--- src/cmds/test/qstat/scaffolding.c
+++ src/cmds/test/qstat/scaffolding.c
@@ -7,6 +7,30 @@
 int pbs_errno = 0;
 char *pbs_server = NULL;
 
+extern "C"
+{
+void set_attr(
+
+  struct attrl **attrib,        /* I */
+  const char   *attrib_name,   /* I */
+  const char   *attrib_value)  /* I */ 
+  
+  {
+  }
+}
+
+struct batch_status * pbs_selstatattr_err(
+
+  int             c,
+  struct attropl *attropl,
+  struct attrl   *attrib,
+  char           *extend,
+  int            *local_errno)
+
+  {
+  return(NULL);
+  }
+
 char *pbs_geterrmsg(int connect)
   { 
   fprintf(stderr, "The call to pbs_geterrmsg needs to be mocked!!\n");
