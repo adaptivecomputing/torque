cfcf114974ca9ea67094890a2643bbe0b43c3134
David Beer <dbeer@adaptivecomputing.com>
2013-06-25 15:00:29 -0600

    Revert the change made for TRQ-1961. This change made it so that I couldn't get my server to listen for connections.

--- src/lib/Libnet/test/get_hostname/scaffolding.c
+++ src/lib/Libnet/test/get_hostname/scaffolding.c
@@ -1,11 +1,10 @@
 #include "license_pbs.h" /* See here for the software license */
 #include <stdlib.h>
 #include <netinet/in.h>
-#include "pbs_error.h"
 
 int insert_addr_name_info(
     
-  const char               *hostname,
+  char               *hostname,
   char               *full_hostname,
   struct sockaddr_in *sai)
 
@@ -15,19 +14,10 @@ int insert_addr_name_info(
 
 char *get_cached_fullhostname(
 
-  const char               *hostname,
-  const struct sockaddr_in *sai)
+  char               *hostname,
+  struct sockaddr_in *sai)
 
   {
   return(NULL);
   }
 
-int pbs_getaddrinfo(
-    
-    const char *pNode,
-    struct addrinfo *pHints,
-    struct addrinfo **ppAddrInfoOut)
-
-  {
-  return(PBSE_NONE);
-  }
