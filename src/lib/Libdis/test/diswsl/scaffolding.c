0a26e631a8c131078bd71921e792a4ad6479249b
bdaw <bdaw@adaptivecomputing.com>
2013-07-19 14:09:27 -0600

    added unit test.

--- src/lib/Libdis/test/diswsl/scaffolding.c
+++ src/lib/Libdis/test/diswsl/scaffolding.c
@@ -2,27 +2,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "tcp.h"
+#include <string>
+
+std::string output;
 
 int tcp_puts(tcp_chan *chan, const char *str, size_t ct)
   {
-  fprintf(stderr, "The call to tcp_puts needs to be mocked!!\n");
-  exit(1);
+  output += str;
+  return ct;
   }
 
 int tcp_wcommit(tcp_chan *chan, int commit_flag)
   {
-  fprintf(stderr, "The call to tcp_wcommit needs to be mocked!!\n");
-  exit(1);
-  }
-
-char *discui_(char *cp, unsigned value, unsigned *ndigs)
-  {
-  fprintf(stderr, "The call to discui_ needs to be mocked!!\n");
-  exit(1);
-  }
-
-char *discul_(char *cp, unsigned long value, unsigned *ndigs)
-  {
-  fprintf(stderr, "The call to discul_ needs to be mocked!!\n");
-  exit(1);
+  return 0;
   }
