290a61b0bf4c90dd04d80a4d97370078e83893c9
Elizabeth Chan <echan@adaptivecomputing.com>
2013-08-09 17:15:02 -0600

    Fix for TRQ-2129: system call write fail if writing too large amounts of data.

--- src/lib/Libifl/tcp_dis.c
+++ src/lib/Libifl/tcp_dis.c
@@ -109,7 +109,7 @@
 #define MAX_SOCKETS 65536
 time_t pbs_tcp_timeout = 300;  
 
-
+#define MAXCHUNK 50000 
 
 void DIS_tcp_settimeout(
 
@@ -312,8 +312,11 @@ int DIS_tcp_wflush(
 
   {
   size_t            ct;
+  size_t            writtenCount = 0;
+  size_t            totalCt, chunk;
   int               i;
   char             *pb = NULL;
+  char             *nextPb = NULL;
   char             *pbs_debug = NULL;
 
   struct tcpdisbuf *tp;
@@ -321,35 +324,45 @@ int DIS_tcp_wflush(
   pbs_debug = getenv("PBSDEBUG");
 
   tp = &chan->writebuf;
-  pb = tp->tdis_thebuf;
-  ct = tp->tdis_trailp - tp->tdis_thebuf;
+  nextPb = tp->tdis_thebuf;
+  totalCt = chunk = tp->tdis_trailp - tp->tdis_thebuf;
 
-  while ((i = write_ac_socket(chan->sock, pb, ct)) != (ssize_t)ct)
+  while (writtenCount < totalCt)
     {
-    if (i == -1)
+    if (chunk > MAXCHUNK)
+      chunk = MAXCHUNK; 
+    ct = chunk;
+    pb = nextPb;
+    while ((i = write_ac_socket(chan->sock, pb, ct)) != (ssize_t)ct)
       {
-      if (errno == EINTR)
+      if (i == -1)
         {
-        continue;
-        }
+        if (errno == EINTR)
+          {
+          continue;
+          }
 
-      /* FAILURE */
+        /* FAILURE */
 
-      if (pbs_debug != NULL)
-        {
-        fprintf(stderr,
-          "TCP write of %d bytes (%.32s) [sock=%d] failed, errno=%d (%s)\n",
-          (int)ct, pb, chan->sock, errno, strerror(errno));
-        }
+        if (pbs_debug != NULL)
+          {
+          fprintf(stderr,
+            "TCP write of %d bytes (%.32s) [sock=%d] failed, errno=%d (%s)\n",
+            (int)ct, pb, chan->sock, errno, strerror(errno));
+          }
       
-      return(-1);
-      }  /* END if (i == -1) */
-    else
-      {
-      ct -= i;
-      pb += i;
-      }
-    }  /* END while (i) */
+        return(-1);
+        }  /* END if (i == -1) */
+      else
+        {
+        ct -= i;
+        pb += i;
+        }
+      }  /* END while (i) */
+    writtenCount += chunk;
+    nextPb += chunk;
+    chunk = totalCt - writtenCount;
+    } /* END while (writtenCount) */
 
   /* SUCCESS */
 
--- src/lib/Libifl/test/tcp_dis/test_tcp_dis.c
+++ src/lib/Libifl/test/tcp_dis/test_tcp_dis.c
@@ -1,35 +1,131 @@
 #include "license_pbs.h" /* See here for the software license */
 #include "lib_ifl.h"
 #include "test_tcp_dis.h"
+#include "dis.h"
+#include "tcp.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "pbs_error.h"
 
-START_TEST(test_one)
+/* Note: must set MAXCHUNK in tcp_dis.c to 30 for an actual test, 
+ * I could not find a value to override it with a test value of 30
+ * even if I had defined MAXCHUNK in a header file and undefinining
+ * in this file and redefine it to 30.
+*/
+
+#ifndef MAXCHUNK
+/* #define MAXCHUNK 30 */
+#define MAXCHUNK 50000
+#endif
+
+char *setup_tcp_chan_for_test(struct tcp_chan *chan, const char *buf)
   {
+  memset(chan, 0, sizeof(struct tcp_chan));
 
+  chan->writebuf.tdis_bufsize = 100;
+  chan->writebuf.tdis_thebuf = (char *) calloc(chan->writebuf.tdis_bufsize, sizeof(char));
+  strcpy(chan->writebuf.tdis_thebuf, buf);
+  chan->writebuf.tdis_leadp = chan->writebuf.tdis_thebuf + strlen(buf); /* not relevant to the unit test */
+  chan->writebuf.tdis_trailp = chan->writebuf.tdis_thebuf + strlen(buf); 
 
+  char *tempfilename = tempnam("/tmp", "test");
+  fail_unless((tempfilename != NULL), "Failed to create a temporary filename");
+  chan->sock = open(tempfilename, O_CREAT | O_WRONLY | O_TRUNC, 0777);
+  fail_unless((chan->sock > 2), "Failed to create a temporary filename for write");
+
+  return tempfilename;
   }
-END_TEST
 
-START_TEST(test_two)
+char *setupAndDoBreakup(const char *strToBreak)
+ {
+ struct tcp_chan chan;
+ char *tempfilename = setup_tcp_chan_for_test(&chan, strToBreak);
+ DIS_tcp_wflush(&chan);
+ close(chan.sock);
+ free(chan.writebuf.tdis_thebuf);
+ return tempfilename;
+}
+
+void test_chunk_breakups(const char *filename, const char *str)
   {
+  FILE *fp = fopen(filename, "r");
+  fail_unless(fp != NULL, "unable to open written file with the breaks");
 
+  char strToBreak[256];
+  snprintf(strToBreak, sizeof(strToBreak), "%s", str);
+  size_t len = strlen(strToBreak);
+  char *buf = strToBreak;
+  char saveChar;
+  size_t processed_chars = 0;
+  char readingBuf[MAXCHUNK + 10];
 
+  int lineRead = 0;
+  int nlines = len/MAXCHUNK + 1; 
+
+  char *endBufStr = buf + strlen(buf);
+
+  while (processed_chars < len)
+    {
+    char *x = fgets(readingBuf, sizeof(readingBuf), fp);
+    fail_unless(x != NULL, "unexpected end of file");
+    if (!x)
+      break;
+
+    size_t l = strlen(readingBuf);
+    if (readingBuf[l - 1] == '\n')
+      readingBuf[l - 1] = '\0';
+    saveChar = '\0';
+    if ((buf + MAXCHUNK) < endBufStr) 
+      {
+      saveChar = *(buf + MAXCHUNK);
+      *(buf + MAXCHUNK) = '\0';
+      }
+    lineRead += 1;
+    int r = strcmp(readingBuf, buf);
+    processed_chars += strlen(buf);
+    if (saveChar) 
+      *(buf + MAXCHUNK) = saveChar;
+    fail_unless(r == 0, "Did not write the chunks we expected", buf);
+    buf += MAXCHUNK;
+    }
+
+    if (lineRead != nlines)
+      {
+      char err_buf[512];
+      snprintf(err_buf, sizeof(err_buf), "Did not write the number of chunks we expected %d %d: %s", lineRead, nlines, str);
+      fail(err_buf);
+      }
+  }
+
+START_TEST(test_DIS_tcp_wflush_writing_chunks)
+  {
+  char buf_len_larger_than_chunk_max[] = "hello there, this is a test of writing a chunk of data. In this case, our chunk lenght is 30";
+  char buf_len_larger_as_much_as_chunk_max[] = "Something of 30, only thirty ch";
+  char buf_len_less_than_chunk_max[] = "shortly - less than 30";
+
+  char *tempfilename = setupAndDoBreakup(buf_len_larger_than_chunk_max);
+  test_chunk_breakups(tempfilename, buf_len_larger_than_chunk_max);
+  unlink(tempfilename);
+
+  tempfilename = setupAndDoBreakup(buf_len_larger_as_much_as_chunk_max);
+  test_chunk_breakups(tempfilename, buf_len_larger_as_much_as_chunk_max);
+  unlink(tempfilename);
+
+  tempfilename = setupAndDoBreakup(buf_len_less_than_chunk_max);
+  test_chunk_breakups(tempfilename, buf_len_less_than_chunk_max);
+  unlink(tempfilename);
   }
 END_TEST
 
 Suite *tcp_dis_suite(void)
   {
   Suite *s = suite_create("tcp_dis_suite methods");
-  TCase *tc_core = tcase_create("test_one");
-  tcase_add_test(tc_core, test_one);
-  suite_add_tcase(s, tc_core);
-
-  tc_core = tcase_create("test_two");
-  tcase_add_test(tc_core, test_two);
+  TCase *tc_core = tcase_create("test_DIS_tcp_wflush_writing_chunks");
+  tcase_add_test(tc_core, test_DIS_tcp_wflush_writing_chunks);
   suite_add_tcase(s, tc_core);
 
   return s;
