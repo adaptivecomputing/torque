cfd03e5830d0f9907771c582784ed22492a97178
David Beer <dbeer@adaptivecomputing.com>
2013-10-04 17:20:44 -0600

    Fix an error from merging TRQ-2124.

--- src/resmom/linux/cpuset.c
+++ src/resmom/linux/cpuset.c
@@ -1158,27 +1158,28 @@ int init_torque_cpuset(void)
 #else
   if (read_cpuset(TTORQUECPUSET_PATH, cpus, mems) == -1)
 #endif
-  {
-  if (errno != ENOENT)
     {
-    /* Error */
-    log_err(errno, __func__, log_buffer);
-    goto finish;
-    }
-  else if (! (hwloc_bitmap_iszero(cpus) || hwloc_bitmap_iszero(mems)))
-    {
-    /* Exists with non-empty cpus and mems, adjust and tell what we have and return */
-    remove_logical_processor_if_requested(&cpus);
-    sprintf(log_buffer, "cpus = ");
-    hwloc_bitmap_displaylist(log_buffer + strlen(log_buffer), sizeof(log_buffer) - strlen(log_buffer), cpus);
-    log_event(PBSEVENT_SYSTEM, PBS_EVENTCLASS_SERVER, __func__, log_buffer);
+    if (errno != ENOENT)
+      {
+      /* Error */
+      log_err(errno, __func__, log_buffer);
+      goto finish;
+      }
+    else if (! (hwloc_bitmap_iszero(cpus) || hwloc_bitmap_iszero(mems)))
+      {
+      /* Exists with non-empty cpus and mems, adjust and tell what we have and return */
+      remove_logical_processor_if_requested(&cpus);
+      sprintf(log_buffer, "cpus = ");
+      hwloc_bitmap_displaylist(log_buffer + strlen(log_buffer), sizeof(log_buffer) - strlen(log_buffer), cpus);
+      log_event(PBSEVENT_SYSTEM, PBS_EVENTCLASS_SERVER, __func__, log_buffer);
 
-    sprintf(log_buffer, "mems = ");
-    hwloc_bitmap_displaylist(log_buffer + strlen(log_buffer), sizeof(log_buffer) - strlen(log_buffer), mems);
-    log_event(PBSEVENT_SYSTEM, PBS_EVENTCLASS_SERVER, __func__, log_buffer);
+      sprintf(log_buffer, "mems = ");
+      hwloc_bitmap_displaylist(log_buffer + strlen(log_buffer), sizeof(log_buffer) - strlen(log_buffer), mems);
+      log_event(PBSEVENT_SYSTEM, PBS_EVENTCLASS_SERVER, __func__, log_buffer);
 
-    rc = 0;
-    goto finish;
+      rc = 0;
+      goto finish;
+      }
     }
 
   /* Add all resources of the root cpuset */
