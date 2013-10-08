c8c6dc8b52badd93f21baaa05ab4b68170347971
David Beer <dbeer@adaptivecomputing.com>
2013-10-08 15:24:55 -0600

    TRQ-2022. Fix a problem with the merging of cpuset changes.

--- src/resmom/linux/cpuset.c
+++ src/resmom/linux/cpuset.c
@@ -1165,21 +1165,21 @@ int init_torque_cpuset(void)
       log_err(errno, __func__, log_buffer);
       goto finish;
       }
-    else if (! (hwloc_bitmap_iszero(cpus) || hwloc_bitmap_iszero(mems)))
-      {
-      /* Exists with non-empty cpus and mems, adjust and tell what we have and return */
-      remove_logical_processor_if_requested(&cpus);
-      sprintf(log_buffer, "cpus = ");
-      hwloc_bitmap_displaylist(log_buffer + strlen(log_buffer), sizeof(log_buffer) - strlen(log_buffer), cpus);
-      log_event(PBSEVENT_SYSTEM, PBS_EVENTCLASS_SERVER, __func__, log_buffer);
+    }
+  else if (! (hwloc_bitmap_iszero(cpus) || hwloc_bitmap_iszero(mems)))
+    {
+    /* Exists with non-empty cpus and mems, adjust and tell what we have and return */
+    remove_logical_processor_if_requested(&cpus);
+    sprintf(log_buffer, "cpus = ");
+    hwloc_bitmap_displaylist(log_buffer + strlen(log_buffer), sizeof(log_buffer) - strlen(log_buffer), cpus);
+    log_event(PBSEVENT_SYSTEM, PBS_EVENTCLASS_SERVER, __func__, log_buffer);
 
-      sprintf(log_buffer, "mems = ");
-      hwloc_bitmap_displaylist(log_buffer + strlen(log_buffer), sizeof(log_buffer) - strlen(log_buffer), mems);
-      log_event(PBSEVENT_SYSTEM, PBS_EVENTCLASS_SERVER, __func__, log_buffer);
+    sprintf(log_buffer, "mems = ");
+    hwloc_bitmap_displaylist(log_buffer + strlen(log_buffer), sizeof(log_buffer) - strlen(log_buffer), mems);
+    log_event(PBSEVENT_SYSTEM, PBS_EVENTCLASS_SERVER, __func__, log_buffer);
 
-      rc = 0;
-      goto finish;
-      }
+    rc = 0;
+    goto finish;
     }
 
   /* Add all resources of the root cpuset */
