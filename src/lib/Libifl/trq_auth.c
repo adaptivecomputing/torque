e0859f5e4ee38feea14437260acb3af4216bd9e1
Ken <knielson@adaptivecomputing.com>
2013-10-11 17:44:37 -0600

    Added new error PBSE_DOMAIN_SOCKET_FAULT so we can know when trqauthd is the problem. TRQ-2265

--- src/lib/Libifl/trq_auth.c
+++ src/lib/Libifl/trq_auth.c
@@ -226,7 +226,7 @@ int get_active_pbs_server(
   if (rc != PBSE_NONE)
     {
     fprintf(stderr, "socket_connect_unix failed: %d\n", rc);
-    return(PBSE_SYSTEM);
+    return(rc);
     }
 
   rc = socket_write(local_socket, write_buf, write_buf_len);
