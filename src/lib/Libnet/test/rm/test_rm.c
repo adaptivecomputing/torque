49f4af747d0dd82b4b643b1384c45e2d488d999b
bdaw <bdaw@adaptivecomputing.com>
2013-11-01 17:13:24 -0600

    Added unit tests for rm.c

--- src/lib/Libnet/test/rm/test_rm.c
+++ src/lib/Libnet/test/rm/test_rm.c
@@ -9,9 +9,10 @@
 #include "pbs_error.h"
 
 extern int debug_read(int sock,char **bf,long long *len);
+extern int debug_write(int sock,char *bf,long long len);
 
 
-START_TEST(test_one)
+START_TEST(test_addreq)
   {
 
   int err = 0;
@@ -34,9 +35,60 @@ START_TEST(test_one)
   }
 END_TEST
 
-START_TEST(test_two)
+START_TEST(test_allreq)
   {
 
+  int err = 0;
+  //struct tcp_chan *chan;
+
+  int sock = openrm((char *)"The Host",322);
+  fail_unless(begin_rm_req(sock,&err,6) == 0);
+  fail_unless(allreq((char *)"hello") == 2);
+  fail_unless(allreq((char *)"to") == 2);
+  fail_unless(allreq((char *)"all") == 2);
+  fail_unless(allreq((char *)"the") == 2);
+  fail_unless(allreq((char *)"boys") == 2);
+  fail_unless(allreq((char *)"and girls") == 2);
+  flushreq();
+  char *bf = NULL;
+  long long len = 0;
+  fail_unless(debug_read(sock,&bf,&len) == 0);
+
+  fail_unless(strcmp(bf,"+1+2+6+2+5hello+2to+3all+3the+4boys+9and girls") == 0);
+  }
+END_TEST
+
+START_TEST(test_closerm)
+  {
+  int sock = openrm((char *)"The Host",322);
+  fail_unless(closerm(sock) == 0);
+
+  char *bf = NULL;
+  long long len = 0;
+  fail_unless(debug_read(sock,&bf,&len) == 0);
+  fail_unless(strcmp(bf,"+1+2+0+1") == 0);
+
+  }
+END_TEST
+
+START_TEST(test_downrm)
+  {
+  int err = 0;
+
+  int sock = openrm((char *)"The Host",322);
+
+  fail_unless(debug_write(sock,(char *)"3+100",5) == 5);
+  fail_unless(downrm(&err,sock) == 0);
+  }
+END_TEST
+
+START_TEST(test_configrm)
+  {
+  int sock = openrm((char *)"The Host",322);
+  int err = 0;
+
+  fail_unless(debug_write(sock,(char *)"3+100",5) == 5);
+  fail_unless(configrm(sock,&err,(char *)"Some file or other") == 0);
 
   }
 END_TEST
@@ -44,12 +96,24 @@ END_TEST
 Suite *rm_suite(void)
   {
   Suite *s = suite_create("rm_suite methods");
-  TCase *tc_core = tcase_create("test_one");
-  tcase_add_test(tc_core, test_one);
+  TCase *tc_core = tcase_create("test_addreq");
+  tcase_add_test(tc_core, test_addreq);
   suite_add_tcase(s, tc_core);
 
-  tc_core = tcase_create("test_two");
-  tcase_add_test(tc_core, test_two);
+  tc_core = tcase_create("test_allreq");
+  tcase_add_test(tc_core, test_allreq);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_closerm");
+  tcase_add_test(tc_core, test_closerm);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_downrm");
+  tcase_add_test(tc_core, test_downrm);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_configrm");
+  tcase_add_test(tc_core, test_configrm);
   suite_add_tcase(s, tc_core);
 
   return s;
