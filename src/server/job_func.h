7a13070139ed1a4173b50a7a4e7266c390f2f525
Ken <knielson@adaptivecomputing.com>
2013-11-13 10:27:12 -0700

    Revert "TRQ-2342: Merge fix from TRQ-2317/TRQ-1714 and added unit test: spool files left alone when MS is the same as pbs_server and there's an epilogue running."
    This reverts commit 405434b094a0e15e413ee2b012921860171f69df.

--- src/server/job_func.h
+++ src/server/job_func.h
@@ -28,7 +28,7 @@ void cleanup_restart_file(struct job *pjob);
 
 int record_jobinfo(struct job *pjob);
 
-int svr_job_purge(struct job *pjob, int keepSpoolFiles=0);
+int svr_job_purge(struct job *pjob);
 
 /*static*/ struct job *find_job_by_array(struct all_jobs *aj, char *job_id, int get_subjob);
 
