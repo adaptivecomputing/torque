77c0969a455d26112dcc6e5452c00963642b7f74
David Beer <dbeer@adaptivecomputing.com>
2014-01-08 14:25:03 -0700

    Fix a unit test failure.

--- src/resmom/test/mom_process_request/scaffolding.c
+++ src/resmom/test/mom_process_request/scaffolding.c
@@ -154,10 +154,8 @@ void req_cpyfile(struct batch_request *preq)
   exit(1);
   }
  
-void req_quejob(struct batch_request *preq)
+void mom_req_quejob(struct batch_request *preq)
   {
-  fprintf(stderr, "The call to req_quejob needs to be mocked!!\n");
-  exit(1);
   }
 
 char *pbse_to_txt(int err)
