a1cada9ada3a4671595ef3783d5650dde905a05a
Ken <knielson@adaptivecomputing.com>
2014-03-06 11:34:51 -0700

    term_job runs in a loop on the same job and calls node_bailout which eventually calls exec_bail. exec_bail does a close on ji_stdout and ji_std_err. exec_bail was already checking to see if these values were > 0 but they were never changed. After closing the files the file descriptors or set to -1 so they cannot be closed again. Coverity issue 1187622

--- src/resmom/start_exec.c
+++ src/resmom/start_exec.c
@@ -630,10 +630,17 @@ void exec_bail(
     exiting_tasks = 1;
 
     if (pjob->ji_stdout > 0)
+      {
       close(pjob->ji_stdout);
+      pjob->ji_stdout = -1;
+      }
 
     if (pjob->ji_stderr > 0)
+      {
       close(pjob->ji_stderr);
+      pjob->ji_stdout = -1;
+      }
+
     }
   else
     {
