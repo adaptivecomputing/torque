fa10c9e88688132d2ce5e661235f293df6889f2f
bdaw <bdaw@adaptivecomputing.com>
2014-05-02 11:32:39 -0600

    Initial energy tracking code with dummy numbers.

--- src/resmom/mom_main.c
+++ src/resmom/mom_main.c
@@ -1971,6 +1971,8 @@ void add_diag_jobs_memory_info(
         resvalue = getsize(pres);
       else if (!(strcmp(resname, "cput")))
         resvalue = gettime(pres);
+      else if (!(strcmp(resname, "energy_used")))
+        resvalue = gettime(pres);
       else
         continue;
 
