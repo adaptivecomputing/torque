95044b4ef7e55432119a9ffa29b69c24d1cd4aaf
David Beer <dbeer@adaptivecomputing.com>
2014-05-07 14:22:33 -0600

    TRQ-2358. Fix a race condition around req_track.
    We weren't protecting the conditionally allocated track data, so now lock it with a mutex.

--- src/server/pbsd_init.c
+++ src/server/pbsd_init.c
@@ -2069,6 +2069,7 @@ int handle_tracking_records()
     return(-1);
     }
 
+  // the request pool hasn't started yet so we won't bother locking
   server.sv_tracksize = (statbuf.st_size + sizeof(struct tracking) - 1) / sizeof(struct tracking);
 
   if (server.sv_tracksize < PBS_TRACK_MINSIZE)
