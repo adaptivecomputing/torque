bc6d11b9eb249f4dd6cb10d2982b99c90211e195
David Beer <dbeer@adaptivecomputing.com>
2014-05-07 11:05:14 -0600

    TRQ-2657. Fix a regression using numa_board_str.
    The first value in numa_board_str was being skipped, making the values off by one from
    the admin-supplied specification.

--- src/server/node_func.c
+++ src/server/node_func.c
@@ -1542,7 +1542,7 @@ int copy_properties(
  * number in val and advances the string to the next number past the comma
  */
 
-static int read_val_and_advance(
+int read_val_and_advance(
 
   int   *val,
   char **str)
@@ -1594,11 +1594,11 @@ static int setup_node_boards(
   char            log_buf[LOCAL_LOG_BUF_SIZE];
 
   if (pnode == NULL)
-  {
-  rc = PBSE_BAD_PARAMETER;
-  log_err(rc, __func__, "NULL input pbsnode poiner");
-  return(rc);
-  }
+    {
+    rc = PBSE_BAD_PARAMETER;
+    log_err(rc, __func__, "NULL input pbsnode poiner");
+    return(rc);
+    }
 
   pnode->parent = NULL;
 
@@ -1613,7 +1613,6 @@ static int setup_node_boards(
   if (pnode->numa_str != NULL)
     {
     np_ptr = pnode->numa_str;
-    read_val_and_advance(&np,&np_ptr);
     }
   else
     np = pnode->nd_slots.get_total_execution_slots() / pnode->num_node_boards;
