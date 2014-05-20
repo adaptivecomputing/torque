9505bd5e4d9cd4052af475ea9d7196b6f5b01537
David Beer <dbeer@adaptivecomputing.com>
2014-05-20 14:41:31 -0600

    TRQ-2548. Fix one more issue reported by valgrind.

--- src/server/node_func.c
+++ src/server/node_func.c
@@ -3633,7 +3633,7 @@ int send_hierarchy(
   char               *string;
   int                 ret = PBSE_NONE;
   int                 sock;
-  struct addrinfo    *pAddrInfo;
+  struct addrinfo    *pAddrInfo = NULL;
   struct sockaddr_in  sa;
   struct tcp_chan    *chan = NULL;
 
