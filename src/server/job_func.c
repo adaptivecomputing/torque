530e8c4aac12ac870112a7d600cf335264f9d3bc
Elizabeth Chan <echan@adaptivecomputing.com>
2014-05-29 13:37:10 -0600

    TRQ-2692: merge from 4.5-dev: xml job log_error.

--- src/server/job_func.c
+++ src/server/job_func.c
@@ -1712,7 +1712,7 @@ int record_jobinfo(
   bf += "<Jobinfo>\n";
   bf += "\t<Job_Id>";
   bf += pjob->ji_qs.ji_jobid;
-  bf += "</JobId>\n";
+  bf += "</Job_Id>\n";
 
  #if 0
   if ((rc = log_job_record(buffer->str)) != PBSE_NONE)
