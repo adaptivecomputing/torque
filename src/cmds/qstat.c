ebaea81d07a9696aeedc3434b554d65a8cf5b70b
Elizabeth Chan <echan@adaptivecomputing.com>
2014-08-13 10:37:23 -0600

    TRQ-2410: Merge from develop.

--- src/cmds/qstat.c
+++ src/cmds/qstat.c
@@ -3284,7 +3284,7 @@ int main(
        break;
 
       }    /* END switch (mode) */
-    if (any_failed != PBSE_NONE)
+    if ((ret_code == PBSE_NONE) && (any_failed != PBSE_NONE))
       {
         ret_code = any_failed;
       }
