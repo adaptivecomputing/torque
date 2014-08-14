ba2354fbebcfc3354e7342dd19ecd5a47b47ecdc
Elizabeth Chan <echan@adaptivecomputing.com>
2014-08-14 10:24:00 -0600

    TRQ-2803: more unit tests fixing.

--- src/server/test/req_track/scaffolding.c
+++ src/server/test/req_track/scaffolding.c
@@ -6,6 +6,7 @@
 #include "server.h" /* server */
 #include "work_task.h" /* work_task, work_type */
 #include "batch_request.h" /* batch_request */
+#include "mutex_mgr.hpp"
 
 char *path_track;
 char server_name[PBS_MAXSERVERNAME + 1];
@@ -65,3 +66,15 @@ ssize_t write_ac_socket(int fd, const void *buf, ssize_t count)
 void log_err(int errnum, const char *routine, const char *text) {}
 void log_record(int eventtype, int objclass, const char *objname, const char *text) {}
 void log_event(int eventtype, int objclass, const char *objname, const char *text) {}
+
+mutex_mgr::mutex_mgr(pthread_mutex_t *, bool a)
+  {
+  }
+
+int mutex_mgr::unlock()
+  {
+  return(0);
+  }
+
+mutex_mgr::~mutex_mgr() {}
+
