9ccc8505a9f3f243263e90d7838aba33e6d26005
Leonid Nikulin <leonid.nikulin@dsr-company.com>
2014-08-21 18:07:50 +0400

    TRQ-1054: added unit test for set_str_scv
    - fix memory leak in remove_csv_item
    - implemented unit test for set_str_csv

--- src/lib/Libattr/test/attr_fn_str/scaffolding.c
+++ src/lib/Libattr/test/attr_fn_str/scaffolding.c
@@ -4,8 +4,15 @@
 
 #include "attribute.h" /* svrattrl */
 
+extern char in_csv;
 int ctnodes(char *spec)
   {
   fprintf(stderr, "The call to append_link needs to be mocked!!\n");
   exit(1);
+  }
+
+char* csv_find_string(char const* val1, char const* val2)
+  {
+  fprintf(stderr, "The call to csv_find_string needs to be mocked!!\n");
+  return (in_csv)? (char*)&in_csv : 0;
   }
