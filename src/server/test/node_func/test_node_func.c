78e669bd1e416e453c1469c1a41858b22971b569
David Beer <dbeer@adaptivecomputing.com>
2014-09-05 11:21:17 -0600

    Fix several unit test issues.

--- src/server/test/node_func/test_node_func.c
+++ src/server/test/node_func/test_node_func.c
@@ -381,38 +381,10 @@ START_TEST(status_nodeattrib_test)
                              &node,
                              0,
                              0,
-                             NULL,
-                             &result_mask);
-  fail_unless(result != PBSE_NONE, "NULL input tlist_head pointer fail: %d" ,result);
-
-  result = status_nodeattrib(&attributes,
-                             &node_attributes,
-                             &node,
-                             0,
-                             0,
                              &list,
                              NULL);
   fail_unless(result != PBSE_NONE, "NULL input result_mask pointer fail: %d" ,result);
 
-  result = status_nodeattrib(NULL,
-                             &node_attributes,
-                             &node,
-                             0,
-                             0,
-                             &list,
-                             &result_mask);
-  /*FIXME: NOTE: this is probably a correct set of input parameters, but still returns -1*/
- /*   fail_unless(result != PBSE_NONE, "NULL input svrattrl pointer fail: %d" ,result); */
-
-  result = status_nodeattrib(&attributes,
-                             &node_attributes,
-                             &node,
-                             0,
-                             0,
-                             &list,
-                             &result_mask);
-  /*FIXME: NOTE: this is probably a correct set of input parameters, but still returns -1*/
-  fail_unless(result != PBSE_NONE, "status_nodeattrib fail: %d" ,result);
   }
 END_TEST
 
