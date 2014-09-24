967cdc80150690459a47a35a658abeee0ca6e5cb
Chad Vizino <cvizino@adaptivecomputing.com>
2014-09-24 07:57:11 -0600

    TRQ-2885 - clean up unit test so it passes when run as root

--- src/lib/Libifl/test/tm/test_tm.c
+++ src/lib/Libifl/test/tm/test_tm.c
@@ -55,9 +55,9 @@ START_TEST(test_tm_adopt_ispidowner)
   /* we are the owner of this pid so should return true */
   fail_unless(TRUE == ispidowner(getpid()));
 
-  /* assuming the unit test is not run as root, owner */
-  /* owner of this pid is not the owner of pid 1 (init) */
-  fail_unless(FALSE == ispidowner(1));
+  /* when unit test run as non-root user, owner of this pid is not the owner of pid 1 (init) */
+  if (getuid() != 0)
+    fail_unless(FALSE == ispidowner(1));
   }
 END_TEST
 
