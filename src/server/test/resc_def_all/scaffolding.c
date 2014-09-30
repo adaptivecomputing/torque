ec030c8e5cf421685a3e028882709654e1b13d74
Elizabeth Chan <echan@adaptivecomputing.com>
2014-09-30 17:44:32 -0600

    TRQ-2892: fixed compilation of unit test for CentOS.

--- src/server/test/resc_def_all/scaffolding.c
+++ src/server/test/resc_def_all/scaffolding.c
@@ -224,3 +224,23 @@ svrattrl *attrlist_create(const char *aname, const char *rname, int vsize)
   {
   return(NULL);
   }
+
+char * csv_find_string(const char *csv_str, const char *search_str)
+  {
+  fprintf(stderr, "The call to csv_find_string to be mocked!!\n");
+  exit(1);
+  }
+
+char *csv_nth(const char *csv_str, int n)
+  {
+  fprintf(stderr, "The call to csv_nth to be mocked!!\n");
+  exit(1);
+  }
+
+int csv_length(const char *csv_str)
+  {
+  fprintf(stderr, "The call to csv_length to be mocked!!\n");
+  exit(1);
+  }
+
+
