f8712fd1e660ce3118093c109cc993e29470cebc
knielson <knielson@adaptivecomputing.com>
2014-10-29 12:57:49 -0600

    fixed unit tests

--- src/server/test/attr_recov/scaffolding.c
+++ src/server/test/attr_recov/scaffolding.c
@@ -1,10 +1,11 @@
 #include "license_pbs.h" /* See here for the software license */
 #include <stdlib.h>
 #include <stdio.h> /* fprintf */
+#include <ctype.h> /* isspace */
 
 #include "list_link.h" /* list_link */
 #include "attribute.h" /* pbs_attribute, attribute_def */
-#include <string>
+#include "dynamic_string.h" /* dynamic_string */
 
 
 ssize_t read_nonblocking_socket(int fd, void *buf, ssize_t count)
@@ -43,21 +44,12 @@ void *get_next(list_link pl, char *file, int line)
   exit(1);
   }
 
-int attr_to_str(std::string&, attribute_def *at_def, struct pbs_attribute attr, int XML)
+int attr_to_str(struct dynamic_string *ds, attribute_def *at_def, struct pbs_attribute attr, int XML)
   {
   fprintf(stderr, "The call to attr_to_str needs to be mocked!!\n");
   exit(1);
   }
 
-/*int ctnodes(
-
-  char *spec)
-
-  {
-  return(1);
-  }*/
-
-#if 0
 
 void clear_dynamic_string(dynamic_string *ds)
   {
@@ -76,10 +68,140 @@ void free_dynamic_string(dynamic_string *ds)
   fprintf(stderr, "The call to attr_to_str needs to be mocked!!\n");
   exit(1);
   }
-#endif
+
+int ctnodes(
+  char *spec
+  )
+  {
+  return(1);
+  }
+
+/**
+ * Gets the number of items in a string list.
+ * @param csv_str  The string list.
+ * @return The number of items in the list.
+ */
+int csv_length(const char *csv_str)
+  {
+  int  length = 0;
+  const char *cp;
+
+  if (!csv_str || *csv_str == 0)
+    return(0);
+
+  length++;
+
+  cp = csv_str;
+
+  while ((cp = strchr(cp, ',')))
+    {
+    cp++;
+    length++;
+    }
+
+  return(length);
+  }
 
 
 
+/**
+ * Gets the nth item from a comma seperated list of names.
+ * @param csv_str  The string list.
+ * @param n The item number requested (0 is the first item).
+ * @return Null if csv_str is null or empty,
+ *     otherwise, a pointer to a local buffer containing the nth item.
+ */
+#define NBUFFERS        32
+char *csv_nth(const char *csv_str, int n)
+  {
+  int  i;
+  const char *cp;
+  char *tp;
+  static char buffer[NBUFFERS][128];
+  static  int     buffer_index;
+
+  if (!csv_str || *csv_str == 0)
+    return(0);
+
+  cp = csv_str;
+
+  for (i = 0; i < n; i++)
+    {
+    if (!(cp = strchr(cp, ',')))
+      {
+      return(0);
+      }
+
+    cp++;
+    }
+
+  buffer_index++;
+
+  if (buffer_index >= NBUFFERS)
+    buffer_index = 0;
+
+  memset(buffer[buffer_index], 0, sizeof(buffer[buffer_index]));
+
+  if ((tp = strchr((char *)cp, ',')))
+    {
+    if ((tp - cp) > 128)
+      return 0;
+    /* Does this need to be null terminated? */
+    strncpy(buffer[buffer_index], cp, tp - cp);
+    }
+  else
+    {
+    snprintf(buffer[buffer_index], 128, "%s", cp);
+    }
+
+  return(buffer[buffer_index]);
+  }
+
+
+/**
+ *
+ * csv_find_string
+ *
+ * Search a csv list for an entry that matches a specified search string.
+ */
+char *csv_find_string(
+    
+  const char *csv_str,
+  const char *search_str)
+
+  {
+  int  i;
+  int  nitems;
+  int  search_length = 0;
+  char *cp;
+
+  if (!search_str)
+    return(NULL);
+
+  search_length = strlen(search_str);
+
+  nitems = csv_length(csv_str);
+
+  for (i = 0; i < nitems; i++)
+    {
+    cp = csv_nth(csv_str, i);
+
+    if (cp)
+      {
+      while (isspace(*cp))
+        cp++;
+
+      if ((int)strlen(cp) >= search_length &&
+          !isalpha(cp[search_length]) &&
+          !strncmp(cp, search_str, search_length))
+        {
+        return(cp);
+        }
+      }
+    }
+
+  return(NULL);
+  }
 extern char saveBuff[];
 extern int saveBuffRdPtr;
 extern int saveBuffEndPtr;
