37815e901a15d7ac83ac84e68df8a1b6d7734d80
David Beer <dbeer@adaptivecomputing.com>
2015-01-26 11:44:17 -0700

    Fix a Coverity bug for logically dead code that shouldn't be logically dead.

--- src/lib/Libifl/trq_auth.c
+++ src/lib/Libifl/trq_auth.c
@@ -842,6 +842,7 @@ int authorize_socket(
   std::string  &message,
   char         *msg_buf,
   char        **server_name_ptr,
+  char        **user_name_ptr,
   std::string  &err_msg)
 
   {
@@ -850,7 +851,6 @@ int authorize_socket(
   int          server_port;
   int          auth_type = 0;
   int          svr_sock = -1;
-  char        *user_name = NULL;
   int          user_pid = 0;
   int          user_sock = 0;
   int          trq_server_addr_len = 0;
@@ -873,13 +873,13 @@ int authorize_socket(
    * 0|0||
    */
 
-  if ((rc = parse_request_client(local_socket, server_name_ptr, &server_port, &auth_type, &user_name, &user_pid, &user_sock)) != PBSE_NONE)
+  if ((rc = parse_request_client(local_socket, server_name_ptr, &server_port, &auth_type, user_name_ptr, &user_pid, &user_sock)) != PBSE_NONE)
     {
     if (*server_name_ptr != NULL)
       free(*server_name_ptr);
 
-    if (user_name != NULL)
-      free(user_name);
+    if (*user_name_ptr != NULL)
+      free(*user_name_ptr);
 
     return(rc);
     }
@@ -893,7 +893,7 @@ int authorize_socket(
       rc = PBSE_NONE;
       disconnect_svr = true;
 
-      if ((rc = validate_user(local_socket, user_name, user_pid, msg_buf)) != PBSE_NONE)
+      if ((rc = validate_user(local_socket, *user_name_ptr, user_pid, msg_buf)) != PBSE_NONE)
         {
         log_record(PBSEVENT_CLIENTAUTH | PBSEVENT_FORCE, PBS_EVENTCLASS_TRQAUTHD, __func__, msg_buf);
         disconnect_svr = false;
@@ -932,7 +932,7 @@ int authorize_socket(
         usleep(50000);
         continue;
         }
-      else if ((rc = build_request_svr(auth_type, user_name, user_sock, message)) != PBSE_NONE)
+      else if ((rc = build_request_svr(auth_type, *user_name_ptr, user_sock, message)) != PBSE_NONE)
         {
         socket_close(svr_sock);
         disconnect_svr = false;
@@ -977,7 +977,7 @@ int authorize_socket(
           }
 
         sprintf(msg_buf,
-          "User %s at IP:port %s:%d logged in", user_name, server_name, server_port);
+          "User %s at IP:port %s:%d logged in", *user_name_ptr, server_name, server_port);
         log_record(PBSEVENT_CLIENTAUTH | PBSEVENT_FORCE, PBS_EVENTCLASS_TRQAUTHD,
           className, msg_buf);
         }
@@ -988,12 +988,9 @@ int authorize_socket(
 
   if (disconnect_svr == true)
     {
-    send_svr_disconnect(svr_sock, user_name);
+    send_svr_disconnect(svr_sock, *user_name_ptr);
     socket_close(svr_sock);
     }
-
-  if (user_name != NULL)
-    free(user_name);
 
   if (trq_server_addr != NULL)
     free(trq_server_addr);
@@ -1073,7 +1070,7 @@ void *process_svr_conn(
 
       case TRQ_AUTH_CONNECTION:
         {
-        rc = authorize_socket(local_socket, message, msg_buf, &server_name, error_string);
+        rc = authorize_socket(local_socket, message, msg_buf, &server_name, &user_name, error_string);
         break;
         }
       default:
