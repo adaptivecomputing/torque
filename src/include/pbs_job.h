646a7892ccb21830ffea29f1d7226f79f0f9b338
David Beer <dbeer@adaptivecomputing.com>
2015-02-17 09:56:29 -0700

    TRQ-3068 & TRQ-3024. Fix a crash due to deleted jobs still being found in the alljobs container.
    There is a stray codepath which allows for this possibility and this patch shores that up.
    
    Conflicts:
    
    	CHANGELOG
    	src/include/pbs_job.h

--- src/include/pbs_job.h
+++ src/include/pbs_job.h
@@ -680,6 +680,9 @@ struct job
   char              ji_being_recycled;
   time_t            ji_last_reported_time;
   time_t            ji_mod_time;       // the timestamp of when the state last changed
+  // This indicates that we've successfully removed the job from the alljobs container.
+  // This is used to prevent potential memory leaks.
+  bool              ji_has_been_removed;
 #endif/* PBS_MOM */   /* END SERVER ONLY */
   int               ji_commit_done;   /* req_commit has completed. If in routing queue job can now be routed */
 
