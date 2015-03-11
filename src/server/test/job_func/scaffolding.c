9c4919a30aa60a7e6694cf294158b16b7809f9c3
Chad Vizino <cvizino@adaptivecomputing.com>
2015-03-10 18:48:01 -0600

    Fix unit test build problems.

--- src/server/test/job_func/scaffolding.c
+++ src/server/test/job_func/scaffolding.c
@@ -509,7 +509,7 @@ job *svr_find_job(const char *jobid, int sub)
   return(NULL);
   }
 
-int remove_job(all_jobs *aj, job             *pjob)
+int remove_job(all_jobs *aj, job             *pjob, bool b)
   {
   return(0);
   }
