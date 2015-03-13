77d4b79d400a843450f091d4a3848397be428f3c
Chad Vizino <cvizino@adaptivecomputing.com>
2015-03-13 17:19:29 -0600

    TRQ-3078 Adjust requested job id during req modify Adjust (shorten if needed) the requested job id during a job modify request. To enable the mom to match job ids, this is necessary when display_server_suffix is set to false on the server.

--- src/server/test/req_modify/test_req_modify.c
+++ src/server/test/req_modify/test_req_modify.c
@@ -4,10 +4,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "pbs_error.h"
-START_TEST(test_one)
+
+batch_request *preq;
+
+extern char *get_correct_jobname_return;
+
+START_TEST(test_req_modifyjob)
   {
+  // initialize
+  get_correct_jobname_return = NULL;
 
+  // create space for the req
+  preq = (batch_request *)calloc(1, sizeof(batch_request));
+  fail_unless(preq != NULL);
 
+  // load a full job name into the req name
+  snprintf(preq->rq_ind.rq_modify.rq_objname, sizeof(preq->rq_ind.rq_modify.rq_objname),
+     "%s", "123.servername");
+
+  // call the function to unit test
+  req_modifyjob(preq);
+
+  // the request objname should be unchanged
+  fail_unless(0 == strncmp(preq->rq_ind.rq_modify.rq_objname, "123.servername",
+    sizeof(preq->rq_ind.rq_modify.rq_objname)));
+
+  // set up get_correct_jobname() to return a different value
+  get_correct_jobname_return = strdup("123");
+
+  // call the function to unit test
+  req_modifyjob(preq);
+
+  // make sure the request objname was changed to the forced value
+  fail_unless(0 == strncmp(preq->rq_ind.rq_modify.rq_objname, get_correct_jobname_return,
+    sizeof(preq->rq_ind.rq_modify.rq_objname)));
   }
 END_TEST
 
@@ -21,8 +51,8 @@ END_TEST
 Suite *req_modify_suite(void)
   {
   Suite *s = suite_create("req_modify_suite methods");
-  TCase *tc_core = tcase_create("test_one");
-  tcase_add_test(tc_core, test_one);
+  TCase *tc_core = tcase_create("test_req_modifyjob");
+  tcase_add_test(tc_core, test_req_modifyjob);
   suite_add_tcase(s, tc_core);
 
   tc_core = tcase_create("test_two");
