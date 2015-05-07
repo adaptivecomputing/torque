11c90cec15183569c44263ee8040de832bc716a5
David Beer <dbeer@adaptivecomputing.com>
2015-05-07 14:26:29 -0600

    TRQ-2825. Make sure the NULL check happens first.

--- src/lib/Libutils/u_xml.c
+++ src/lib/Libutils/u_xml.c
@@ -105,23 +105,21 @@ int get_parent_and_child(
   char **end)         /* O */
 
   {
+  if (start == NULL)
+    return(-1);
+
   char *ptr = strchr(start,'<');
   char  closingTag[MAXLINE];
   int   parent_len = 0;
   char *child_start;
   char *child_end;
   int   reached_space = FALSE;
-
-  // Stop looking at NULL or an empty string
-  if (start == NULL)
+  
+  // Make sure the string contains more than just whitespace
+  *end = trim(start);
+  start = *end;
+  if (*start == '\0')
     return(-1);
-  else
-    {
-    *end = trim(start);
-    start = *end;
-    if (*start == '\0')
-      return(-1);
-    }
 
   if (ptr == NULL)
     {
