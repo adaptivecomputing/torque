638e554eda6239ea875b5ac5ca5f3e9bf6db91fd
knielson <knielson@adaptivecomputing.com>
2015-05-07 11:07:39 -0600

    fixed unit test for queue_recov

--- src/server/test/queue_recov/scaffolding.c
+++ src/server/test/queue_recov/scaffolding.c
@@ -90,3 +90,33 @@ void log_err(int errnum, const char *routine, const char *text) {}
 void log_record(int eventtype, int objclass, const char *objname, const char *text) {}
 void log_event(int eventtype, int objclass, const char *objname, const char *text) {}
 
+char *trim(
+
+  char *str) /* M */
+
+  {
+  char *front_ptr;
+  char *end_ptr;
+
+  if (str == NULL)
+    return(NULL);
+
+  front_ptr = str;
+  end_ptr = str + strlen(str) - 1;
+
+  while ((is_whitespace(*front_ptr) == TRUE) &&
+         (front_ptr <= end_ptr))
+    {
+    front_ptr++;
+    }
+
+  while ((is_whitespace(*end_ptr) == TRUE) &&
+         (end_ptr > front_ptr))
+    {
+    *end_ptr = '\0';
+    end_ptr--;
+    }
+
+  return(front_ptr);
+  } /* END trim() */
+
