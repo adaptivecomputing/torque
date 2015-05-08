aa740743f442fe2799f284bfb1ed8dd062a9593c
knielson <knielson@adaptivecomputing.com>
2015-05-08 15:22:04 -0600

    TRQ-2761 momctl warnings about communicating with server -- Cray 20753

--- src/resmom/mom_server.c
+++ src/resmom/mom_server.c
@@ -1663,7 +1663,15 @@ void mom_server_all_update_stat(void)
       if (buf[0] != '0')
         num_stat_update_failures++;
       else
+        {
         num_stat_update_failures = 0;
+        for (int sindex = 0; sindex < PBS_MAXSERVER; sindex++)
+          {
+          if (mom_servers[sindex].pbs_servername[0] == '\0')
+            continue;
+          mom_servers[sindex].MOMLastSendToServerTime = time_now;
+          }
+        }
 
       return;
       }
