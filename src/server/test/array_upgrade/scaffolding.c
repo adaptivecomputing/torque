451e813f0932f0a4f92adc58b7e84952beb8af13
David Beer <dbeer@adaptivecomputing.com>
2015-06-01 10:57:41 -0600

    Fix a unit test complaint.

--- src/server/test/array_upgrade/scaffolding.c
+++ src/server/test/array_upgrade/scaffolding.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h> /* fprintf */
 
+char  *path_arrays;
+
 void log_err(int errnum, const char *routine, const char *text) {}
 void log_record(int eventtype, int objclass, const char *objname, const char *text) {}
 void log_event(int eventtype, int objclass, const char *objname, const char *text) {}
