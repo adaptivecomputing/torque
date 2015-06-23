28b426bf5270f04c7bf746c8bff10cd3e5374ecd
David Beer <dbeer@adaptivecomputing.com>
2015-06-23 11:25:35 -0600

    Fix two more scaffolding problems.

--- src/server/test/job_func/scaffolding.c
+++ src/server/test/job_func/scaffolding.c
@@ -623,3 +623,4 @@ bool completed_jobs_map_class::add_job(char const* s, time_t t)
 
 std::string get_path_jobdata(const char *a, const char *b) {return ""; }
 
+void add_to_completed_jobs(work_task *ptask) {}
