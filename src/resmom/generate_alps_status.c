57efdc56965ed7c180b1a6826626a1c475bf9ce8
David Beer <dbeer@adaptivecomputing.com>
2015-06-23 11:03:33 -0600

    TRQ-2839. Improve the logging of node states from ALPS.
    At level 3 or higher, log a message every time a node state changes.
    At level 7 or higher, log the state from ALPS every time.

--- src/resmom/generate_alps_status.c
+++ src/resmom/generate_alps_status.c
@@ -87,8 +87,11 @@
 #include "alps_constants.h"
 #include <string>
 #include <vector>
+#include <set>
 
 #include "../lib/Libifl/lib_ifl.h"
+
+std::set<std::string> down_nodes;
 
 
 /*
@@ -333,6 +336,9 @@ int process_node(
   xmlNode            *compute_units;
   xmlNode            *compute_unit_child;
   std::string        features = "";
+  std::string        node_state = "";
+  std::string        node_name = "";
+  std::string        node_id_string  = "";
   char                buf[MAXLINE];
   int                 num_procs         = 0;
   int                 avail_procs       = 0;
@@ -340,10 +346,12 @@ int process_node(
   unsigned long       memory            = 0;
   unsigned long long  mem_kb;
   char               *rsv_id        = NULL;
+  std::set<std::string>::iterator iter;
 
     {
     std::string str("node=");
     attr_value = (char *)xmlGetProp(node, (const xmlChar *)node_id);
+    node_id_string = attr_value;
     str += attr_value;
     status.push_back(str);
     free(attr_value);
@@ -363,6 +371,7 @@ int process_node(
     {
     std::string str("name=");
     attr_value = (char *)xmlGetProp(node, (const xmlChar *)name);
+    node_name = attr_value;
     str += attr_value;
     status.push_back(str);
     free(attr_value);
@@ -486,6 +495,7 @@ int process_node(
     free(rsv_id);
 
     /* if there's a reservation on this node, the state is busy */
+    node_state = "BUSY";
     status.push_back("state=BUSY");
     
     snprintf(buf, sizeof(buf), "availmem=0kb");
@@ -497,18 +507,24 @@ int process_node(
     std::string str("state=");
     attr_value = (char *)xmlGetProp(node, (const xmlChar *)state);
     
+
+
     if ((role_value != NULL) &&
         (!strcmp(role_value, interactive_caps)))
       {
+      node_state = "DOWN";
       str += "DOWN";
       status.push_back(str);
       
+
+
       snprintf(buf, sizeof(buf), "availmem=0kb");
       status.push_back(buf);
       }
     else
       {
       str += attr_value;
+      node_state = attr_value;
       status.push_back(str);
      
       snprintf(buf, sizeof(buf), "availmem=%llukb", mem_kb);
@@ -516,6 +532,44 @@ int process_node(
       }
 
     free(attr_value);
+    }
+
+  /* Record whenever an ALPS node is reported as entering a DOWN state */
+  if (LOGLEVEL >= 3)
+    {
+    iter = down_nodes.find(node_id_string);
+
+    /* Check to see if a node's state has changed. All nodes in a DOWN state are
+     * recorded in the down_nodes structure. We can see if the node has changed status
+     * based on whether its ID can be found in the down_nodes structure.
+     */
+    if (node_state == "DOWN")
+      {
+      if (iter == down_nodes.end())
+        {
+        down_nodes.insert(node_id_string);
+
+        snprintf(buf, sizeof(buf), "ALPS node %s (%s) is reported as being DOWN", node_name.c_str(), node_id_string.c_str());
+        log_record(PBSEVENT_SYSTEM, PBS_EVENTCLASS_NODE, __func__, buf);
+        }
+      }
+    else
+      {
+      if (iter != down_nodes.end())
+        {
+        snprintf(buf, sizeof(buf), "ALPS node %s (%s) is online again", node_name.c_str(), node_id_string.c_str());
+        log_record(PBSEVENT_SYSTEM, PBS_EVENTCLASS_NODE, __func__, buf);
+        down_nodes.erase(iter);
+        }
+      }
+
+    }
+
+  /* Verbose debug output, log every single ALPS node's state */
+  if (LOGLEVEL >= 7)
+    {
+    snprintf(buf, sizeof(buf), "ALPS node %s (%s) is reported as being %s", node_name.c_str(), node_id_string.c_str(), node_state.c_str());
+    log_record(PBSEVENT_SYSTEM, 0, __func__, buf);
     }
 
   if (role_value != NULL)
