0017d3cfc534ab941a5b983c44a4cb6df842f20c
knielson <knielson@adaptivecomputing.com>
2015-09-04 17:21:44 -0600

    TRQ-3220 Torque auto_node_np does not override internal proc count of node
    There is a global variable srv_clnodes which is a count of all of the
    execution slots in the cluster. This value was not getting updated so
    on a single node it is easy to expose the problem. Torque does a check
    to see if it is even possible for the job request to every run. If the
    ppn requested is larger than srv_clnodes then it will fail. Because
    this value wasn't updated then the request was too large.

--- src/server/node_func.c
+++ src/server/node_func.c
@@ -2639,11 +2639,13 @@ int node_np_action(
           {
           delete_a_subnode(pnode);
           old_np--;
+          svr_clnodes--;
           }
         else
           {
           add_execution_slot(pnode);
           old_np++;
+          svr_clnodes++;
           }
         }
 
