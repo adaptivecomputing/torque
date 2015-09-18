e966caccb920e3adc7a6b81ac59d2812465fe33c
David Beer <dbeer@adaptivecomputing.com>
2015-09-18 15:27:50 -0600

    TRQ-330. Add support for pbsnodes -o/c all or ALL.

--- src/server/req_manager.c
+++ src/server/req_manager.c
@@ -1655,6 +1655,10 @@ void mgr_node_set(
   struct pbsnode  **problem_nodes = NULL;
   struct prop       props;
 
+  if ((strcmp(preq->rq_ind.rq_manager.rq_objname, "all") == 0) ||
+      (strcmp(preq->rq_ind.rq_manager.rq_objname, "ALL") == 0))
+    strcpy(preq->rq_ind.rq_manager.rq_objname, ":ALL");
+
   if ((*preq->rq_ind.rq_manager.rq_objname == '\0') ||
       (*preq->rq_ind.rq_manager.rq_objname == '@') ||
       ((*preq->rq_ind.rq_manager.rq_objname == ':') &&
