9e7d5913360525b4f222b351546b092c46b3ef29
David Beer <dbeer@adaptivecomputing.com>
2015-10-19 11:23:31 -0600

    Fix some scaffolding issues.

--- src/test/pbsnode/scaffolding.c
+++ src/test/pbsnode/scaffolding.c
@@ -1,1 +1,77 @@
 #include "pbs_nodes.h"
+#include "id_map.hpp"
+#include "u_tree.h"
+#include "mom_hierarchy_handler.h"
+
+AvlTree                 ipaddrs = NULL;
+int                     LOGLEVEL;
+id_map                  node_mapper;
+mom_hierarchy_handler   hierarchy_handler; //The global declaration.
+bool                    exit_called;
+
+int pbs_getaddrinfo(
+    
+  const char       *pNode,
+  struct addrinfo  *pHints,
+  struct addrinfo **ppAddrInfoOut)
+
+  {
+  return(0);
+  }
+
+void log_record(
+
+  int         eventtype,  /* I */
+  int         objclass,   /* I */
+  const char *objname,    /* I */
+  const char *text)       /* I */
+
+  {
+  }
+
+int id_map::get_new_id(const char *nodename)
+  {
+  return(0);
+  }
+
+struct prop *init_prop(
+
+  const char *pname) /* I */
+
+  {
+  return(NULL);
+  }
+
+void free_prop_list(struct prop *prop)
+  {
+  }
+
+struct pbsnode *next_host(
+
+  all_nodes           *an,    /* I */
+  all_nodes_iterator **iter,  /* M */
+  struct pbsnode      *held)  /* I */
+
+  {
+  return(NULL);
+  }
+
+AvlTree AVL_delete_node(
+  u_long   key,
+  uint16_t port,
+  AvlTree  tree)
+
+  {
+  return(NULL);
+  }
+
+void log_err(
+
+  int         errnum,  /* I (errno or PBSErrno) */
+  const char *routine, /* I */
+  const char *text)    /* I */
+
+  {
+  }
+
+
