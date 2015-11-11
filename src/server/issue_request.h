342306ccf252bd65829754c3d98e0bd780bbdc84
David Beer <dbeer@adaptivecomputing.com>
2015-11-11 16:26:54 -0700

    TRQ-3316. Fix a crash in send_depend_req

--- src/server/issue_request.h
+++ src/server/issue_request.h
@@ -10,8 +10,6 @@
 
 void reissue_to_svr(struct work_task *);
 
-int issue_to_svr(char *servern, struct batch_request *preq, void (*replyfunc)(struct work_task *));
-
 int handle_local_request(int conn, batch_request *request);
 
 void release_req(struct work_task *pwt);
