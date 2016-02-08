346fcb38bfd5c4b195a7e00e4d71ee4e8108aa8e
David Beer <dbeer@adaptivecomputing.com>
2016-02-08 11:46:27 -0700

    TRQ-3090. Fix the mom's port being appended to JB files on the mom in an error case.

--- src/resmom/mom_main.c
+++ src/resmom/mom_main.c
@@ -5962,6 +5962,11 @@ void check_jobs_in_mom_wait()
 
   {
   job    *pjob;
+  unsigned int momport = 0;
+
+  if (multi_mom)
+    momport = pbs_rm_port;
+  
   time_now = time(NULL);
   std::list<job *>::iterator iter;
 
@@ -5984,7 +5989,7 @@ void check_jobs_in_mom_wait()
 
         pjob->ji_qs.ji_substate = JOB_SUBSTATE_EXITING;
 
-        job_save(pjob, SAVEJOB_QUICK, pbs_rm_port);
+        job_save(pjob, SAVEJOB_QUICK, momport);
 
         exiting_tasks = 1;
         }
