3c4fb1f97ad85ebde82265b2c33debf742150041
David Beer <dbeer@adaptivecomputing.com>
2016-04-12 14:35:13 -0600

    TRQ-3273. Add a mail point for any non-zero exit code.

--- src/cmds/qsub_functions.c
+++ src/cmds/qsub_functions.c
@@ -2702,13 +2702,14 @@ int process_opt_m(
       if ((*pc != 'a') &&
           (*pc != 'b') &&
           (*pc != 'e') &&
+          (*pc != 'f') &&
           (*pc != 'p'))
         return(-1);
 
       pc++;
       }
     } /* END if (strcmp(cmd_arg,"n") != 0) */
-  else if (strcmp(cmd_arg, "p") != 0)
+  if (strcmp(cmd_arg, "p") != 0)
     {
     const char *pc = cmd_arg;
     
@@ -2716,12 +2717,15 @@ int process_opt_m(
       {
       if ((*pc != 'a') &&
           (*pc != 'b') &&
-          (*pc != 'e'))
+          (*pc != 'e') &&
+          (*pc != 'f') &&
+          (*pc != 'n'))
         return(-1);
 
       pc++;
       }
     } /* END if (strcmp(cmd_arg,"p") != 0) */
+    
           
   hash_add_or_exit(ji->job_attr, ATTR_m, cmd_arg, data_type);
 
