da68435fe0e721eef44421e1ca2a12f0e630eedd
knielson <knielson@adaptivecomputing.com>
2016-04-27 18:03:08 -0600

    TRQ-3588 Syntax error fixed

--- src/include/pbs_error_db.h
+++ src/include/pbs_error_db.h
@@ -286,6 +286,7 @@ PbsErrClient(PBSE_CANT_EDIT_NODES, (char *)"With dont_write_nodes_file set you c
 PbsErrClient(PBSE_GHOSTQUEUE, (char *)"This queue had errors during its recovery. Please correct any settings that were lost on restart and then unset the ghost_queue setting via qmgr. Once this is unset, then the queue will be able to accept new jobs again.") 
 PbsErrClient(PBSE_GPU_NOT_INITIALIZED, (char *)"NVIDIA GPU not initialized")
 PbsErrClient(PBSE_UNSUPPORTED_DEVICE_VERSION, (char *)"Device version not supported")
+PbsErrClient(PBSE_GPU_PROHIBITED_MODE, (char *)"Invalid gpu mode requested. Prohibited mode is not allowed. Check the spelling of the mode request for errors")
 /* pbs client errors ceiling (max_client_err + 1) */
 PbsErrClient(PBSE_CEILING,           (char*)0)
 #endif
