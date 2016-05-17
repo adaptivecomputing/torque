b3ec32222a72c793d447cbf55469989ceae15fb1
David Beer <dbeer@adaptivecomputing.com>
2016-05-17 17:06:38 -0600

    TRQ-3582. Fix some problems with resolving hostnames and HA for client commands.
    Conflicts:
    	src/test/get_server/Makefile.am

--- src/lib/Libcmds/test/get_server/test_get_server.c
+++ src/lib/Libcmds/test/get_server/test_get_server.c
@@ -7,10 +7,39 @@
 
 #include "pbs_error.h"
 
-START_TEST(test_one)
+int get_server(const char *job_id_in, char *job_id_out, int job_id_out_size, char *server_out, int server_out_size);
+
+
+START_TEST(test_get_server)
   {
+  char jid[1024];
+  char server[1024];
 
+  memset(server, 0, sizeof(server));
 
+  fail_unless(get_server("1.napali", jid, sizeof(jid), server, sizeof(server)) == PBSE_NONE);
+  // Specifying a job id with server shouldn't set the server or HA gets hosed
+  fail_unless(strlen(server) == 0);
+  // If they specify a full job id take exactly what they specified
+  fail_unless(!strcmp(jid, "1.napali"));
+  
+  fail_unless(get_server("1@napali", jid, sizeof(jid), server, sizeof(server)) == PBSE_NONE);
+  // @ should get the server set
+  fail_unless(!strcmp(server, "napali"));
+  // default server is set to return "napali"
+  fail_unless(!strcmp(jid, "1.napali"));
+  
+  fail_unless(get_server("1@waimea", jid, sizeof(jid), server, sizeof(server)) == PBSE_NONE);
+  // @ should get the server set
+  fail_unless(!strcmp(server, "waimea"));
+  // default server is set to return "napali"
+  fail_unless(!strcmp(jid, "1.napali"));
+  
+  fail_unless(get_server("1.napali.ac", jid, sizeof(jid), server, sizeof(server)) == PBSE_NONE);
+  // @ should get the server set
+  fail_unless(strlen(server) == 0);
+  // If they specify a full job id take exactly what they specified
+  fail_unless(!strcmp(jid, "1.napali.ac"));
   }
 END_TEST
 
@@ -24,8 +53,8 @@ END_TEST
 Suite *get_server_suite(void)
   {
   Suite *s = suite_create("get_server_suite methods");
-  TCase *tc_core = tcase_create("test_one");
-  tcase_add_test(tc_core, test_one);
+  TCase *tc_core = tcase_create("test_get_server");
+  tcase_add_test(tc_core, test_get_server);
   suite_add_tcase(s, tc_core);
 
   tc_core = tcase_create("test_two");
