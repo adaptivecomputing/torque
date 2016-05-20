7308dc3c5469418374e156001956827579ceff56
David Beer <dbeer@adaptivecomputing.com>
2016-05-20 14:45:24 -0600

    Fix two scaffolding errors.

--- src/test/numa_pci_device/scaffolding.c
+++ src/test/numa_pci_device/scaffolding.c
@@ -1,16 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#ifdef PENABLE_LINUX_CGROUPS
 #include <hwloc.h>
 #include <machine.hpp>
-#include <hwloc.h>
+#endif
+
+#ifdef NVML_API
 #include <hwloc/nvml.h>
 #include <nvml.h>
+#endif
 
 
+#ifdef PENABLE_LINUX_CGROUPS
 hwloc_bitmap_t hwloc_bitmap_alloc()
   {
   return(0);
   }
+#endif
 
 
 void log_err(
