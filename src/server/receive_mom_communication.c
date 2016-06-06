fd1175bd2c7d6653c0e3ee803d0e9c8b1257ecfe
David Beer <dbeer@adaptivecomputing.com>
2016-06-06 17:03:06 -0600

    Fix a crash with pbs_server -e.
    Conflicts:
    	src/server/receive_mom_communication.c

--- src/server/receive_mom_communication.c
+++ src/server/receive_mom_communication.c
@@ -402,9 +402,7 @@ void *svr_is_request(
 
     if (err == PBSE_NONE)
       {
-      node = AVL_find(ipaddr, 0, ipaddrs);
-       
-      node->lock_node(__func__, "no error", LOGLEVEL);
+      node = find_nodebyname(nodename);
       }                                                         
     }
     
