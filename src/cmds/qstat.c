d22f92068fbb5144ba00be62525b31bed8ac5541
Isaac Tobler <itobler@adaptivecomputing.com>
2016-08-05 19:30:09 -0600

    We need DCGM functions/structs/etc to be available to the server; also fixed some compilation errors
    Conflicts:
    	src/include/pbs_job.h
    	src/server/job_attr_def.c

--- src/cmds/qstat.c
+++ src/cmds/qstat.c
@@ -2222,13 +2222,11 @@ void create_full_job_xml(
         {
         print_req_information(attribute, RE1);
         }
-#ifdef NVIDIA_DCGM
       else if (!strcmp(attribute->name, ATTR_dcgm_gpu_use) && 
          (!strcmp(attribute->resource, "summary") || (!strncmp(attribute->resource, "GPU:", 4))))
         {
         print_dcgm_gpu_use(attribute, RE1);
         }
-#endif
       else
         {
 
@@ -2309,12 +2307,10 @@ void display_full_job(
 
           prt_attr(attribute->name, attribute->resource, ctime(&epoch));
           }
-#ifdef NVIDIA_DCGM
         else if (!strcmp(attribute->name, ATTR_dcgm_gpu_use))
           {
           print_dcgm_gpu_use(attribute, NULL);
           }
-#endif
         else
           {
           if ((!strcmp(attribute->name, "Walltime")) && (attribute->value[0] == '-'))
@@ -2326,7 +2322,6 @@ void display_full_job(
         }
       }
     }
-#ifdef NVIDIA_DCGM
   else
     {
     printf("{  \"Job_Id\": \"%s\",\n", p->name);
@@ -2343,7 +2338,6 @@ void display_full_job(
     
     printf("}\n");
     }
-#endif
   } // END display_full_job()
 
 
