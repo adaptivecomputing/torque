500dec2b2e0c328f7b54c7ba14e37c9047119105
Chad Vizino <cvizino@adaptivecomputing.com>
2016-09-30 16:18:53 -0600

    TRQ-3795 - properly start/stop daemons under systemd

--- src/test/trq_auth_daemon/test_uut.c
+++ src/test/trq_auth_daemon/test_uut.c
@@ -1,6 +1,7 @@
 #include "license_pbs.h" /* See here for the software license */
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -8,11 +9,64 @@
 #include "test_trq_auth_daemon.h"
 #include "pbs_error.h"
 
+extern bool daemonize_server;
 
-START_TEST(test_load_trqauthd_config)
+void parse_command_line(int, char**);
+
+bool are_we_forking()
+
   {
+  char *forking = getenv("CK_FORK");
+
+  if ((forking != NULL) &&  
+      (!strcasecmp(forking, "no")))
+    return(false);
+
+  return(true);
+  }
+
+void set_optind()
+
+  {
+  if (are_we_forking() == false)
+    optind = 0;
+  }
+
+START_TEST(test_parse_command_line1)
+  {
+  char *argv[] = {strdup("trqauthd"), strdup("-D")};
+  set_optind();
+
+  daemonize_server = true;
+  parse_command_line(2, argv);
+  fail_unless(daemonize_server == false);
   }
 END_TEST
+
+
+START_TEST(test_parse_command_line2)
+  {
+  char *argv[] = {strdup("trqauthd"), strdup("-F")};
+  set_optind();
+
+  daemonize_server = true;
+  parse_command_line(2, argv);
+  fail_unless(daemonize_server == false);
+  }
+END_TEST
+
+
+START_TEST(test_parse_command_line3)
+  {
+  char *argv[] = {strdup("trqauthd")};
+  set_optind();
+
+  daemonize_server = true;
+  parse_command_line(1, argv);
+  fail_unless(daemonize_server == true);
+  }
+END_TEST
+
 
 START_TEST(test_two)
   {
@@ -22,8 +76,16 @@ END_TEST
 Suite *trq_auth_daemon_suite(void)
   {
   Suite *s = suite_create("trq_auth_daemon_suite methods");
-  TCase *tc_core = tcase_create("test_load_trqauthd_config");
-  tcase_add_test(tc_core, test_load_trqauthd_config);
+  TCase *tc_core = tcase_create("test_parse_command_line1");
+  tcase_add_test(tc_core, test_parse_command_line1);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_parse_command_line2");
+  tcase_add_test(tc_core, test_parse_command_line2);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_parse_command_line3");
+  tcase_add_test(tc_core, test_parse_command_line3);
   suite_add_tcase(s, tc_core);
 
   tc_core = tcase_create("test_two");
