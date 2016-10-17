19767ae5ac708ab433d571217abc0f9fe03bb504
David Beer <dbeer@adaptivecomputing.com>
2016-10-17 17:25:08 -0600

    Commit the two new files as part of the json change.

---
+++ src/lib/Libutils/numa_constants.cpp
@@ -0,0 +1,30 @@
+
+#include "numa_constants.h"
+
+
+
+const char *LPROCS     = "lprocs";
+const char *NODE       = "node";
+const char *NODES      = "nodes";
+const char *SOCKET     = "socket";
+const char *SOCKETS    = "sockets";
+const char *NUMA_NODE  = "numanode";
+const char *NUMA_NODES = "numanodes";
+const char *CORE       = "core";
+const char *CORES      = "cores";
+const char *THREAD     = "thread";
+const char *THREADS    = "threads";
+const char *MEMORY     = "memory";
+const char *SWAP       = "swap";
+const char *DISK       = "disk";
+
+const char *ALLOCATION = "allocation";
+const char *ALLOCATIONS = "allocations";
+const char *MEM         = "mem";
+const char *GPUS        = "gpus";
+const char *MICS        = "mics";
+const char *CORES_ONLY  = "cores_only";
+const char *JOBID       = "jobid";
+const char *CPUS        = "cpus";
+const char *EXCLUSIVE   = "exclusive";
+const char *OS_INDEX    = "os_index";
