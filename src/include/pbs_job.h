f848ac8773230297f321df03b6280dda6bb690d5
David Beer <dbeer@adaptivecomputing.com>
2016-11-18 11:46:37 -0700

    Fix a race-condition in cleaning up jobs.

--- src/include/pbs_job.h
+++ src/include/pbs_job.h
@@ -1307,7 +1307,7 @@ job         *job_clone(job *,struct job_array *, int, bool);
 job         *svr_find_job(const char *jobid, int get_subjob);
 job         *svr_find_job_by_id(int internal_job_id);
 job         *find_job_by_array(all_jobs *aj, const char *job_id, int get_subjob, bool locked);
-bool         job_id_exists(const std::string &job_id_string, int *rcode);
+bool         job_id_exists(const std::string &job_id_string);
 bool         internal_job_id_exists(int internal_id);
 #else
 extern job  *mom_find_job(const char *);
