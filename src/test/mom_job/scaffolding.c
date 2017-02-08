9593d49aaa106554d9aad2718f41b1eec0e9bf2a
David Beer <dbeer@adaptivecomputing.com>
2017-02-07 17:15:09 -0700

    Fix another cent6 scaffolding issue.

--- src/test/mom_job/scaffolding.c
+++ src/test/mom_job/scaffolding.c
@@ -9,3 +9,39 @@ task::~task() {}
 attribute_def job_attr_def[100];
 
 void nodes_free(mom_job *pj) {}
+
+int csv_length(const char *csv_str)
+  {
+  return(0);
+  }
+
+char *csv_nth(const char *csv_str, int n)
+  {
+  return(NULL);
+  }
+
+char* remove_from_csv(
+
+  char* src,            /* I - line with csv values*/
+  char* model_pattern   /* I - pattern with models*/)
+  {
+  return(NULL);
+  }
+
+char *csv_find_string(
+    
+  const char *csv_str,
+  const char *search_str)
+
+  {
+  return(NULL);
+  }
+
+int ctnodes(
+
+  const char *spec)
+
+  {
+  return(0);
+  }
+
