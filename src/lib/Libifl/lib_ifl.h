194e2b2c26c029f49a778d152058bccd7768c2ab
David Beer <dbeer@adaptivecomputing.com>
2017-02-21 14:02:16 -0700

    Slight cleanup in a header file.

--- src/lib/Libifl/lib_ifl.h
+++ src/lib/Libifl/lib_ifl.h
@@ -1,7 +1,6 @@
 #include "license_pbs.h" /* See here for the software license */
 
 #include <netinet/in.h> /* in_addr_t */
-#include "attribute.h" /* attropl, attrl */
 #include "libpbs.h" /* job_file */
 #include "batch_request.h" /* batch_request */
 #include "tm_.h" /* tm_task_id, tm_node_id, tm_event_t */
