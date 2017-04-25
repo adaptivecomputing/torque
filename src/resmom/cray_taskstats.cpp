97c81b23fc8d3d51f044076ecad4a49426386afd
David Beer <dbeer@adaptivecomputing.com>
2017-04-25 11:32:54 -0600

    TRQ-3995. Take the first step towards being able to parse RUR for usage information on the Cray.

---
+++ src/resmom/cray_taskstats.cpp
@@ -0,0 +1,103 @@
+
+#include <pbs_config.h>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+
+#include "pbs_job.h"
+#include "resource.h"
+#include "pbs_error.h"
+#include "resmon.h"
+
+/*
+ * parse_rur_line()
+ *
+ * Parses a line of an rur file to find the stats for this job. 
+ * @param line - the line of the file
+ * @param pjob - the job whose stats we're finding
+ */
+
+// RUR usage lines look like this
+// 2013-11-02T11:09:49.457770-05:00 c0-0c1s1n2 RUR 2417 p0-201311-1t153028 [RUR@34] uid: 12345, apid: 86989, jobid: 0, cmdname: /lus/tmp/rur01.2338/./CPU01-2338 plugin: taskstats ['utime', 10000000, 'stime', 0, 'max_rss', 940, 'rchar', 107480, 'wchar', 90, 'exitcode:signal', ['0:0'], 'core', 0]
+
+
+int parse_rur_line(
+
+  const std::string &line,
+  job               *pjob)
+
+  {
+  size_t pos = line.find("taskstats");
+
+  if (pos != std::string::npos)
+    {
+    if (line.at(pos) == ' ')
+      pos++;
+    if (line.at(pos) == '[')
+      pos++;
+
+    std::string   stats = line.substr(pos, std::string::npos);
+    char         *work_str = strdup(stats.c_str());
+    char         *ptr = strstr(work_str, "utime',");
+    long          cput = 0;
+    long long     mem = 0;
+    resource_def *rd;
+    resource     *pres;
+
+    if (ptr != NULL)
+      {
+      ptr += 7; // move past "utime',"
+
+      if (*ptr == ' ')
+        ptr++;
+
+      cput = strtol(ptr, &ptr, 10);
+      }
+
+    if ((ptr = strstr(ptr, "stime',")) != NULL)
+      {
+      ptr += 7; // move past "stime',"
+      
+      if (*ptr == ' ')
+        ptr++;
+
+      cput += strtol(ptr, &ptr, 10);
+      }
+
+    if (cput > 0)
+      {
+      rd = find_resc_def(svr_resc_def, "cput", svr_resc_size);
+      pres = add_resource_entry(pjob->ji_wattr + JOB_ATR_resc_used, rd);
+      pres->rs_value.at_flags |= ATR_VFLAG_SET;
+      pres->rs_value.at_type = ATR_TYPE_LONG;
+      pres->rs_value.at_val.at_long = MAX(cput, pres->rs_value.at_val.at_long);
+      }
+
+    if ((ptr = strstr(ptr, "max_rss',")) != NULL)
+      {
+      ptr += 9; // move past "max_rss'," 
+
+      if (*ptr == ' ')
+        ptr++;
+
+      mem = strtoll(ptr, NULL, 10);
+
+      if (mem > 0)
+        {
+        rd = find_resc_def(svr_resc_def, "mem", svr_resc_size);
+        pres = add_resource_entry(pjob->ji_wattr + JOB_ATR_resc_used, rd);
+        pres->rs_value.at_flags |= ATR_VFLAG_SET;
+        pres->rs_value.at_type = ATR_TYPE_SIZE;
+        pres->rs_value.at_val.at_size.atsv_shift = 10;
+        pres->rs_value.at_val.at_size.atsv_units = ATR_SV_BYTESZ;
+        pres->rs_value.at_val.at_size.atsv_num = MAX(mem, pres->rs_value.at_val.at_size.atsv_num);
+        }
+      }
+
+    free(work_str);
+    }
+
+  return(PBSE_NONE);
+  } // parse_rur_line()
+
+
---
+++ src/test/cray_taskstats/test_uut.c
@@ -0,0 +1,95 @@
+#include "license_pbs.h" /* See here for the software license */
+#include <pbs_config.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "pbs_error.h"
+#include "pbs_job.h"
+#include "resource.h"
+
+#include <check.h>
+
+int parse_rur_line(const std::string &line, job *pjob);
+const char *l1 = "2013-11-02T11:09:49.457770-05:00 c0-0c1s1n2 RUR 2417 p0-201311-1t153028 [RUR@34] uid: 12345, apid: 86989, jobid: 0, cmdname: /lus/tmp/rur01.2338/./CPU01-2338 plugin: taskstats ['utime', 100, 'stime', 100, 'max_rss', 1984, 'rchar', 107480, 'wchar', 90, 'exitcode:signal', ['0:0'], 'core', 0]";
+const char *l2 = "2013-11-02T11:09:49.457770-05:00 c0-0c1s1n2 RUR 2417 p0-201311-1t153028 [RUR@34] uid: 12345, apid: 86989, jobid: 0, cmdname: /lus/tmp/rur01.2338/./CPU01-2338 plugin: taskstats ['utime', 300, 'stime', 0, 'max_rss', 4444, 'rchar', 107480, 'wchar', 90, 'exitcode:signal', ['0:0'], 'core', 0]";
+
+START_TEST(test_parse_rur_line)
+  {
+  job pjob;
+
+  memset(&pjob, 0, sizeof(pjob));
+  fail_unless(parse_rur_line(l1, &pjob) == PBSE_NONE);
+  resource_def *rd = find_resc_def(svr_resc_def, "cput", svr_resc_size);
+  resource *pres = add_resource_entry(pjob.ji_wattr + JOB_ATR_resc_used, rd);
+  fail_unless(pres != NULL);
+  fail_unless(pres->rs_value.at_val.at_long == 200);
+
+  rd = find_resc_def(svr_resc_def, "mem", svr_resc_size);
+  pres = add_resource_entry(pjob.ji_wattr + JOB_ATR_resc_used, rd);
+  fail_unless(pres != NULL);
+  fail_unless(pres->rs_value.at_val.at_size.atsv_num == 1984,
+    "%llu", pres->rs_value.at_val.at_size.atsv_num);
+ 
+  // Make sure we increase the value if we are using more later
+  fail_unless(parse_rur_line(l2, &pjob) == PBSE_NONE);
+  rd = find_resc_def(svr_resc_def, "cput", svr_resc_size);
+  pres = add_resource_entry(pjob.ji_wattr + JOB_ATR_resc_used, rd);
+  fail_unless(pres != NULL);
+  fail_unless(pres->rs_value.at_val.at_long == 300);
+
+  rd = find_resc_def(svr_resc_def, "mem", svr_resc_size);
+  pres = add_resource_entry(pjob.ji_wattr + JOB_ATR_resc_used, rd);
+  fail_unless(pres != NULL);
+  fail_unless(pres->rs_value.at_val.at_size.atsv_num == 4444);
+
+  // Make sure the values don't decrease if we find less later
+  fail_unless(parse_rur_line(l1, &pjob) == PBSE_NONE);
+  rd = find_resc_def(svr_resc_def, "cput", svr_resc_size);
+  pres = add_resource_entry(pjob.ji_wattr + JOB_ATR_resc_used, rd);
+  fail_unless(pres != NULL);
+  fail_unless(pres->rs_value.at_val.at_long == 300);
+
+  rd = find_resc_def(svr_resc_def, "mem", svr_resc_size);
+  pres = add_resource_entry(pjob.ji_wattr + JOB_ATR_resc_used, rd);
+  fail_unless(pres != NULL);
+  fail_unless(pres->rs_value.at_val.at_size.atsv_num == 4444);
+  }
+END_TEST
+
+START_TEST(test_two)
+  {
+
+
+  }
+END_TEST
+
+Suite *cray_taskstats_suite(void)
+  {
+  Suite *s = suite_create("cray_taskstats_suite methods");
+  TCase *tc_core = tcase_create("test_parse_rur_line");
+  tcase_add_test(tc_core, test_parse_rur_line);
+  suite_add_tcase(s, tc_core);
+
+  tc_core = tcase_create("test_two");
+  tcase_add_test(tc_core, test_two);
+  suite_add_tcase(s, tc_core);
+
+  return s;
+  }
+
+void rundebug()
+  {
+  }
+
+int main(void)
+  {
+  int number_failed = 0;
+  SRunner *sr = NULL;
+  rundebug();
+  sr = srunner_create(cray_taskstats_suite());
+  srunner_set_log(sr, "cray_taskstats_suite.log");
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return number_failed;
+  }
