54f21e0cf51886f40b1906d68646e562fa6806ab
Chad Vizino <cvizino@adaptivecomputing.com>
2017-10-26 10:50:53 -0600

    TRQ-3978 - use NVML API rather than hwloc for info about NVIDIA devices

--- src/lib/Libutils/numa_pci_device.cpp
+++ src/lib/Libutils/numa_pci_device.cpp
@@ -68,7 +68,44 @@ PCI_Device::~PCI_Device()
 
 int PCI_Device::initializePCIDevice(hwloc_obj_t device_obj, int idx, hwloc_topology_t topology)
   {
-  if (device_obj != NULL)
+  // see if we are initializing an NVIDIA PCI device only
+  if (device_obj == NULL)
+    {
+#if defined(NVIDIA_GPUS) && defined(NVML_API)
+    // use NVML library to look up info about the device
+
+    nvmlDevice_t gpu;
+    char *buf;
+
+    // get the device handle so the name can be looked up
+    if (nvmlDeviceGetHandleByIndex(idx, &gpu) != NVML_SUCCESS)
+      return(-1);
+
+    // allocate space for the name
+    if ((buf = (char *)malloc(NVML_DEVICE_PCI_BUS_ID_BUFFER_SIZE)) == NULL)
+      return(-1);
+
+    // look up the name
+    if (nvmlDeviceGetName(gpu, buf, NVML_DEVICE_PCI_BUS_ID_BUFFER_SIZE) != NVML_SUCCESS)
+      {
+      free(buf);
+      return(-1);
+      }
+
+    // copy the name
+    name = buf;
+
+    // free the buffer
+    free(buf);
+
+    // copy the id
+    id = idx;
+#else
+    // NULL hwloc object passed in
+    return(-1);
+#endif
+    }
+  else
     {
     id = device_obj->logical_index;
     name = device_obj->name;
@@ -78,19 +115,13 @@ int PCI_Device::initializePCIDevice(hwloc_obj_t device_obj, int idx, hwloc_topol
       info_value = device_obj->infos->value;
       }
     }
-  else
-    {
-    id = idx;
-    name = "Unknown";
-    }
-
 
 #ifdef MIC
   this->initializeMic(idx, topology);
 #endif
 
 #ifdef NVIDIA_GPUS
-  this->initializeGpu(idx, topology);
+  this->initializeGpu(idx);
 #endif
   return(PBSE_NONE);
   }
