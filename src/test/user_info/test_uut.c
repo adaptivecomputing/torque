315a744cef27ce0224ecddc063080202fb877db0
Chad Vizino <cvizino@adaptivecomputing.com>
2018-04-16 15:59:12 -0600

    TRQ-4186 - correctly increment/decrement user job counts for array jobs

--- src/test/user_info/test_uut.c
+++ src/test/user_info/test_uut.c
@@ -6,6 +6,8 @@
 #include "user_info.h"
 #include <check.h>
 
+user_info_holder queue_users;
+
 unsigned int get_num_queued(user_info_holder *, const char *);
 unsigned int count_jobs_submitted(job *);
 
@@ -31,6 +33,8 @@ END_TEST
 START_TEST(get_num_queued_test)
   {
   unsigned int queued;
+
+  // users
   users.lock();
   users.clear();
   users.unlock();
@@ -48,6 +52,21 @@ START_TEST(get_num_queued_test)
 
   queued = get_num_queued(&users, "tom");
   fail_unless(queued == 1, "incorrect queued count for tom");
+
+  // queue_users
+  queue_users.lock();
+  queue_users.clear();
+  queue_users.unlock();
+
+  ui.user_name = (char *)"jerry";
+  ui.num_jobs_queued = 1;
+
+  queue_users.lock();
+  queue_users.insert(&ui,ui.user_name);
+  queue_users.unlock();
+
+  queued = get_num_queued(&queue_users, "jerry");
+  fail_unless(queued == 1, "incorrect queued count for jerry");
   }
 END_TEST
 
@@ -80,6 +99,8 @@ START_TEST(can_queue_new_job_test)
   job pjob;
 
   memset(&pjob, 0, sizeof(pjob));
+
+  // users
   users.lock();
   users.clear();
 
@@ -105,6 +126,24 @@ START_TEST(can_queue_new_job_test)
 
   fail_unless(can_queue_new_job((char *)"bob", &pjob) == FALSE, "array job allowed over limit");
   fail_unless(can_queue_new_job((char *)"tom", &pjob) == FALSE, "array job allowed over limit");
+
+  // queue_users
+  queue_users.lock();
+  queue_users.clear();
+
+  ui = (user_info *)calloc(1,sizeof(user_info));
+
+  ui->user_name = strdup("jerry");
+  ui->num_jobs_queued = 1;
+
+  queue_users.insert(ui,ui->user_name);
+  queue_users.unlock();
+
+
+  fail_unless(can_queue_new_job((char *)"jerry", &pjob) == FALSE, (char *)"jerry allowed over limit");
+  pjob.ji_wattr[JOB_ATR_job_array_request].at_val.at_str = (char *)"0-10";
+
+  fail_unless(can_queue_new_job((char *)"jerry", &pjob) == FALSE, "array job allowed over limit");
   }
 END_TEST
 
@@ -115,6 +154,8 @@ START_TEST(increment_queued_jobs_test)
   job pjob;
 
   memset(&pjob, 0, sizeof(pjob));
+
+  // users
   users.lock();
   users.clear();
 
@@ -133,20 +174,55 @@ START_TEST(increment_queued_jobs_test)
   users.insert(ui,ui->user_name);
   users.unlock();
 
+  // queue_users
+  queue_users.lock();
+  queue_users.clear();
+
+  ui = (user_info *)calloc(1,sizeof(user_info));
+
+  ui->user_name = strdup("jerry");
+  ui->num_jobs_queued = 1;
+
+  queue_users.insert(ui,ui->user_name);
+  queue_users.unlock();
+
+  // users and queue_users
+  // ensure bit is reset correctly after incrementing
   fail_unless(increment_queued_jobs(&users, (char *)"tom", &pjob) == 0, "can't increment queued jobs");
-  pjob.ji_queue_counted = 0;
-  fail_unless(increment_queued_jobs(&users, (char *)"bob", &pjob) == 0, "can't increment queued jobs");
-  pjob.ji_queue_counted = 0;
-  // after 1 increment the count should be 2 because initialize_user_info() starts out with tom at 
-  // 1 instead of 0, as a normal program would start. Its done this way for the decrement code.
+  fail_unless(increment_queued_jobs(&queue_users, (char *)"jerry", &pjob) == 0, "can't increment queued jobs");
+  fail_unless(pjob.ji_queue_counted == (COUNTED_GLOBALLY | COUNTED_IN_QUEUE));
+  //  after 1 increment the count should be 2 because initialize_user_info() starts out with tom and jerry at 1
   fail_unless(get_num_queued(&users, "tom") == 2, "didn't actually increment tom 1");
+  fail_unless(get_num_queued(&queue_users, "jerry") == 2, "didn't actually increment tom 1");
+  pjob.ji_queue_counted = 0;
+
+  // users
+  fail_unless(increment_queued_jobs(&users, (char *)"bob", &pjob) == 0, "can't increment queued jobs");
+  fail_unless(get_num_queued(&users, "bob") == 1, "didn't actually increment bob 0");
+  pjob.ji_queue_counted = 0;
+  // after 2 increments the count should be 3 because initialize_user_info() starts out with tom at 
+  // 1 instead of 0, as a normal program would start. Its done this way for the decrement code.
   fail_unless(increment_queued_jobs(&users, strdup("tom@napali"), &pjob) == 0);
   fail_unless(get_num_queued(&users, "tom") == 3, "didn't actually increment tom 2");
 
+  // queue_users
+  pjob.ji_queue_counted = 0;
+  fail_unless(increment_queued_jobs(&queue_users, (char *)"jerry", &pjob) == 0, "can't increment queued jobs");
+  fail_unless(get_num_queued(&queue_users, "jerry") == 3, "didn't actually increment jerry 2");
+
+  // users
   // Enqueue the job without resetting to make sure the count doesn't change
+  pjob.ji_queue_counted = COUNTED_GLOBALLY;
   fail_unless(increment_queued_jobs(&users, strdup("tom@napali"), &pjob) == 0);
   fail_unless(get_num_queued(&users, "tom") == 3, "Shouldn't have incremented with the bit set");
 
+  // queue_users
+  // Enqueue the job without resetting to make sure the count doesn't change
+  pjob.ji_queue_counted = COUNTED_IN_QUEUE;
+  fail_unless(increment_queued_jobs(&queue_users, strdup("jerry@napali"), &pjob) == 0);
+  fail_unless(get_num_queued(&queue_users, "jerry") == 3, "Shouldn't have incremented with the bit set");
+
+  // users
   // Make sure array subjobs are a no-op
   pjob.ji_queue_counted = 0;
   pjob.ji_is_array_template = FALSE;
@@ -159,6 +235,23 @@ START_TEST(increment_queued_jobs_test)
   pjob.ji_wattr[JOB_ATR_job_array_id].at_flags = 0;
   fail_unless(increment_queued_jobs(&users, strdup("tom"), &pjob) == 0);
   fail_unless(get_num_queued(&users, "tom") == 4, "Should have incremented for non array sub-job");
+
+  // test array job
+
+  // set array string
+  pjob.ji_wattr[JOB_ATR_job_array_request].at_val.at_str = strdup("0-10");
+
+  // users
+  pjob.ji_queue_counted = 0;
+  pjob.ji_is_array_template = TRUE;
+  fail_unless(increment_queued_jobs(&users, strdup("tom"), &pjob) == 0);
+  fail_unless(get_num_queued(&users, "tom") == 15, "didn't actually increment tom 15");
+
+  // queue_users
+  pjob.ji_queue_counted = 0;
+  pjob.ji_is_array_template = TRUE;
+  fail_unless(increment_queued_jobs(&queue_users, strdup("jerry"), &pjob) == 0);
+  fail_unless(get_num_queued(&queue_users, "jerry") == 14, "didn't actually increment jerry 14");
   }
 END_TEST
 
@@ -166,6 +259,7 @@ END_TEST
 
 START_TEST(decrement_queued_jobs_test)
   {
+  // users
   users.lock();
   users.clear();
   user_info *ui = (user_info *)calloc(1,sizeof(user_info));
@@ -178,6 +272,20 @@ START_TEST(decrement_queued_jobs_test)
   users.insert(ui,ui->user_name);
   users.unlock();
 
+  // queue users
+  queue_users.lock();
+  queue_users.clear();
+  ui = (user_info *)calloc(1,sizeof(user_info));
+
+  ui->user_name = strdup("jerry");
+  ui->num_jobs_queued = 1;
+  memset(&pjob, 0, sizeof(pjob));
+
+  queue_users.insert(ui,ui->user_name);
+  queue_users.unlock();
+
+
+  // users
   pjob.ji_queue_counted = COUNTED_GLOBALLY;
   fail_unless(decrement_queued_jobs(&users, (char *)"bob", &pjob) == THING_NOT_FOUND, "decremented for non-existent user");
   pjob.ji_queue_counted = COUNTED_GLOBALLY;
@@ -195,6 +303,55 @@ START_TEST(decrement_queued_jobs_test)
   fail_unless(get_num_queued(&users, "tom") == 1);
   fail_unless(decrement_queued_jobs(&users, (char *)"tom", &pjob) == 0);
   fail_unless(get_num_queued(&users, "tom") == 0);
+
+  // queue_users
+  pjob.ji_queue_counted = COUNTED_IN_QUEUE;
+  fail_unless(decrement_queued_jobs(&queue_users, (char *)"jerry@neverland.com", &pjob) == 0, "couldn't decrement for jerry?");
+  fail_unless(get_num_queued(&queue_users, "jerry") == 0, "didn't actually decrement jerry");
+
+  fail_unless(decrement_queued_jobs(&queue_users, (char *)"jerry@neverland.com", &pjob) == 0, "couldn't decrement for jerry?");
+  fail_unless(get_num_queued(&queue_users, "jerry") == 0, "preventing redecrementng the same job twice");
+
+  pjob.ji_queue_counted = COUNTED_IN_QUEUE;
+  fail_unless(decrement_queued_jobs(&queue_users, (char *)"jerry", &pjob) == 0, "couldn't decrement for jerry?");
+  fail_unless(get_num_queued(&queue_users, "jerry") == 0, "Disallow going negative in the count");
+
+  // Make sure we are clearing and resetting the bits  
+  fail_unless(increment_queued_jobs(&queue_users, strdup("jerry@napali"), &pjob) == 0);
+  fail_unless(get_num_queued(&queue_users, "jerry") == 1);
+  fail_unless(decrement_queued_jobs(&queue_users, (char *)"jerry", &pjob) == 0);
+  fail_unless(get_num_queued(&queue_users, "jerry") == 0);
+
+  // test array job
+
+  // set array string
+  pjob.ji_wattr[JOB_ATR_job_array_request].at_val.at_str = strdup("0-10");
+
+  // users
+  // first increment
+  pjob.ji_queue_counted = 0;
+  pjob.ji_is_array_template = TRUE;
+  fail_unless(increment_queued_jobs(&users, strdup("tom"), &pjob) == 0);
+  fail_unless(get_num_queued(&users, "tom") == 11);
+
+  // now decrement
+  pjob.ji_queue_counted = COUNTED_GLOBALLY;
+  pjob.ji_is_array_template = TRUE;
+  fail_unless(decrement_queued_jobs(&users, (char *)"tom", &pjob) == 0);
+  fail_unless(get_num_queued(&users, "tom") == 0);
+
+  // queue_users
+  // first increment
+  pjob.ji_queue_counted = 0;
+  pjob.ji_is_array_template = TRUE;
+  fail_unless(increment_queued_jobs(&queue_users, strdup("jerry"), &pjob) == 0);
+  fail_unless(get_num_queued(&queue_users, "jerry") == 11);
+
+  // now decrement
+  pjob.ji_queue_counted = COUNTED_IN_QUEUE;
+  pjob.ji_is_array_template = TRUE;
+  fail_unless(decrement_queued_jobs(&queue_users, (char *)"jerry", &pjob) == 0);
+  fail_unless(get_num_queued(&queue_users, "jerry") == 0);
 
   }
 END_TEST
